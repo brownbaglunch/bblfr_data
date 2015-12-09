fd7f92bec8fdc3259ac79bb8e2c68b4fa9e82120
Fabrice Sznajderman <Fabszn@gmail.com>
2015-12-09 14:12:35 +0100

    fix typo

--- baggers.js
+++ baggers.js
@@ -7047,7 +7047,7 @@ var data = {
             "sessions": [
                 {
                     "title": "Scala de 0 à l'infini",
-                    "summary": "Cette présentation a pour objectif de poser les bases du language Scala et de monter graduellement ans les tours pour aborder des fonctionnalités plus avancées du languages. Après cette présentation, les termes implicit, monade, fonction partielle, pattern matching et closure ne devrait plus avoir de secret pour vous. Le seul pré-requis est de connaitre un language de programmation."
+                    "summary": "Cette présentation a pour objectif de poser les bases du language Scala et de monter graduellement dans les tours pour aborder des fonctionnalités plus avancées du languages. Après cette présentation, les termes implicit, monade, fonction partielle, pattern matching et closure ne devrait plus avoir de secret pour vous. Le seul pré-requis est de connaitre un language de programmation."
                 },
                 {
                     "title": "SBT et pourquoi pas?",
