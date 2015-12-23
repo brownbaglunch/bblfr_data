48624eddf5eefeca6be0a486d308fd45ac4fe12c
Youen Chéné <youen.chene@gadz.org>
2015-12-23 09:04:20 +0100

    Adding a new BBL proposal for Rouen & Paris

--- baggers.js
+++ baggers.js
@@ -7077,6 +7077,37 @@ var data = {
             "cities": [
                 "Paris"
             ]
+        },
+        {
+            "name": "Youen Céhné",
+            "bio": "CTO @ Creative Data",
+            "picture": "https://www.creativedata.fr/bundles/app/img/team/youen.png",
+            "websites": [
+                {
+                    "title": "Blog",
+                    "href": "http://www.youenchene.fr/"
+                }
+            ],
+            "twitter": "youen_chene",
+            "contact": "youen.chene@gadz.org",
+            "mail": "youen.chene@gadz.org",
+            "location": "Rouen",
+            "sessions": [
+                {
+                    "title": "Le Big Data pour les Javaistes et le reste d’entre nous",
+                    "summary": "Le Big Data, c’est pour nombre d’entre vous c’est une pile de buzz words sur des slides. Pourtant le big data c’est en place chez les startups depuis longtemps et cela fonctionnait même avant que Spark envahisse les sujets de conférences.Pendant une heure, on fera un tour d’horizon des différentes technologies (pas forcément à la mode) qui vous permettront de profiter des avantages des outils du Big Data. Pour chaque technologie, on verra les points principaux et surtout un exemple avec du code. Au programme : hdfs, impala, spark, scoop, R etc…"
+                }
+            ],
+            "tags": [
+                "Big Data",
+                "Hadoop",
+                "Impala",
+                "Spark"
+            ],
+            "cities": [
+                "Paris",
+                "Rouen"
+            ]
         }
     ],
     "cities": [
