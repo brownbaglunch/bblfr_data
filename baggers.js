e12671f33251cea91cd7859ec297aa32d2a18dc2
farrault <farrault@ippon.fr>
2016-01-10 19:24:27 +0100

    Update baggers.js

--- baggers.js
+++ baggers.js
@@ -5627,6 +5627,32 @@ var data = {
             ]
         },
         {
+            "name": "Fabien Arrault",
+            "bio": "Architecte JavaEE chez Ippon Technologies",
+            "picture": "https://drive.google.com/file/d/0B-ShMcwHfbkDcVhUM3RCc1FnZGs/view?usp=sharing",
+            "websites": [
+                {"title": "Blog", "href": "http://blog.ippon.fr/author/Fabien_ARRAULT/"},
+                {"title": "LinkedIn", "href": "https://fr.linkedin.com/in/fabien-arrault-b52b324"}
+            ],
+            "twitter": "farrault",
+            "contact": "farrault [at] ippon.fr",
+            "mail": "farrault@ippon.fr",
+            "location": "Paris",
+            "sessions": [
+                {
+                    "title": "Spring Boot : on ouvre le capot et on démonte tout !",
+                    "summary": "Spring Boot facilite grandement la mise en place d'une application utilisant l'écosystème Spring. Oui mais voilà son usage se rapproche souvent de la magie : ça marche, mais difficile de savoir comment... Lorsqu'il s'agit de tuner un peu de la configuration, de la débugguer, on se retrouve potentiellement devant un mur... Le but de cette conférence est de rentrer dans les mécanismes internes de Spring Boot, essentiels pour qui veut pouvoir maîtriser entièrement la bête. On parlera de @Bean et de @Configuration bien sûr, mais aussi de @Conditional, d'@AutoConfiguration et de @ConfigurationProperties"
+                }
+            ],
+            "tags": [
+                "Spring",
+                "Spring Boot"
+            ],
+            "cities": [
+                "Paris"
+            ]
+        },
+        {
             "name": "Manuel Raynaud",
             "bio": "@theliaecommerce developer and PHP addict",
             "picture": "http://www.manuel-raynaud.com/wp-content/uploads/2013/12/manuel.jpg",
