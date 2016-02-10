9e5a0d20559ea6f55bf7aa522e30e2fb9b9778db
Pierre Leresteux <pierre.leresteux@gmail.com>
2016-02-10 22:28:24 +0100

    Ajout de moi dans la liste des "baggers"

--- baggers.js
+++ baggers.js
@@ -7450,6 +7450,29 @@ var data = {
             "cities": [
                 "Paris"
             ]
+        },
+        {
+            "since": "2016-02-10",
+            "name": "Pierre Leresteux",
+            "bio": "Développeur / Coach Agile @ Creative Data / Saagie, activiste @codeursenseine @devoxx4kidsfr",
+            "picture": "https://secure.gravatar.com/avatar/8f43dd546e83248e7763ad3b38b893b4?size=200",
+            "websites": [],
+            "twitter": "pierreleresteux",
+            "contact": "pierre.leresteux@gmail.com",
+            "mail": "pierre.leresteux@gmail.com",
+            "location": "Rouen",
+            "sessions": [
+                {
+                    "title": "Docker au quotidien : améliorons la vie des développeurs avec des conteneurs",
+                    "summary": "Docker par-ci, docker par-là … Le phénomène Docker prend de plus en plus de poids dans notre écosystème. Je vous montrerai que Docker n’est pas fait que pour déployer des applications en production. Pour aider au développement, pour valider des architectures, pour tester des scripts de déploiement, tout peut être prétexte à utiliser Docker et à créer des conteneurs. On découvrira les principes de base de Docker (qu’est-ce qu’une image ? quelle est la différence avec un conteneur ? quelles sont les commandes utiles ?), puis à l’aide de cas concrets (et vécus !) on vera comment utiliser Docker pour développer plus facilement. On parlera ensuite des outils complémentaires permettant d’automatiser et de faciliter l’utilisation de Docker"
+                }
+            ],
+            "tags": [
+                "Docker"
+            ],
+            "cities": [
+                "Rouen"
+            ]
         }
     ],
     "cities": [
