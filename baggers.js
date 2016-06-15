ca9e5f879d9e64f9cd6c88b975c180e42bb8ecbe
David Pilato <david@pilato.fr>
2016-06-15 11:58:25 +0200

    Update David Pilato

--- baggers.js
+++ baggers.js
@@ -386,21 +386,21 @@ var data = {
     {
       "since": "2013-02-15",
       "name": "David Pilato",
-      "bio": "Technical Advocate",
-      "picture": "http://www.gravatar.com/avatar/20049a614b7f6838dc87b4bc39c42079.png",
+      "bio": "Developer | Evangelist",
+      "picture": "http://www.gravatar.com/avatar/20049a614b7f6838dc87b4bc39c42079.png?s=200",
       "websites": [
         {
           "name": "Web",
-          "url": "http://dev.david.pilato.fr/"
+          "url": "http://david.pilato.fr/"
         }
       ],
       "location": "Régions francophones",
       "sessions": [
         {
           "title": "Elasticsearch",
-          "abstract": "Vous utilisez encore des requêtes SQL pour faire des recherches ? Vos utilisateurs vous reprochent de ne pas pouvoir chercher sur toutes les rubriques ? Votre temps de réponse moyen est supérieur à la demi-seconde avec seulement quelques millions de documents ? Il vous faut 3 jours pour produire des statistiques sur vos données ? Vous rêvez d’offrir une recherche 'à la google' sur les données de votre SI ?<br/><br/>Ne cherchez plus ! Cette conférence est faite pour vous.<br/><br/>David présentera au cours de du BBL pourquoi et comment il est passé de la recherche SQL à Elasticsearch en détaillant les apports de ce moteur par rapport à une solution pure Lucene :<ul><li>Pourquoi Elasticsearch ?</li><li>L'indexation</li><li>La recherche</li><li>La collecte de données (rivers)</li><li>Les facettes et le principe de navigation par facettes</li><li>La scalabilité horizontale</li><li>L'analyse et le mapping</li><li>La percolation</li><li>La communauté</li></ul>",
+          "abstract": "Vous utilisez encore des requêtes SQL pour faire des recherches ? Vos utilisateurs vous reprochent de ne pas pouvoir chercher sur toutes les rubriques ? Votre temps de réponse moyen est supérieur à la demi-seconde avec seulement quelques millions de documents ? Il vous faut 3 jours pour produire des statistiques sur vos données ? Vous rêvez d’offrir une recherche 'à la google' sur les données de votre SI ?<br/><br/>Ne cherchez plus ! Cette conférence est faite pour vous.<br/><br/>David présentera au cours de du BBL pourquoi et comment il est passé de la recherche SQL à Elasticsearch en détaillant les apports de ce moteur par rapport à une solution pure Lucene :<ul><li>Pourquoi Elasticsearch ?</li><li>L'indexation</li><li>La recherche</li><li>Les agrégations et le principe de navigation par facettes</li><li>La scalabilité horizontale</li><li>L'analyse et le mapping</li><li>La percolation</li><li>La communauté</li></ul>",
           "tags": [
-            "ElasticSearch"
+            "Elasticsearch"
           ],
           "lang": [
             "fr"
