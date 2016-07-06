4bce8ac8f1cd6b3eb2f021b0dd1220e12ad401ee
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2016-07-06 08:14:03 +0200

    Nouveau BBL: Craftmanship et Lego
    Ajout d’un BBL destiné à la découverte du Craftmanship à travers les
    lego

--- baggers.js
+++ baggers.js
@@ -7201,6 +7201,19 @@ var data = {
           "lang": [
             "fr"
           ]
+        },
+        {
+          "title": "Le Craftsmanship par les Lego",
+          "abstract": "Cet atelier propose de découvrir le clean code, refactoring, dette technique, approche iérative grâces à des exerices de construction. Adaptée à tous les profils, cette session est un moyen formidable de sensibiliser des équipes, sponsors, métiers aux bénéfices du Craftsmanship.",
+          "tags": [
+            "CleanCode",
+            "TDD",
+            "Iteratif",
+            "Refactoring"
+          ],
+          "lang": [
+            "fr"
+          ]
         }
       ],
       "cities": [
