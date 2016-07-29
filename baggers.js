0b02d390fafc9fad0627e0700e2557c6f47c9711
fredericpot <fpot@lucca.fr>
2016-07-29 16:49:28 +0200

    Update baggers.js

--- baggers.js
+++ baggers.js
@@ -13299,6 +13299,42 @@ var data = {
         "twitter": "gallain",
         "mail": "guillaume@llain.fr"
       }
+    },
+    {
+      "since": "2016-07-18",
+      "name": "Frédéric Pot",
+      "bio": "Co-fondateur de Lucca en 2002, je suis le Product Owner de la solution Figgo utilisée par 115 000 salariés de 900 sociétés à travers 30 pays. Je serai ravi de vous partager mon expérience sur les 15 dernières années autour de l’évolution dans la gestion d’un logiciel RH.",
+      "picture": "https://avatars1.githubusercontent.com/u/2751422?v=3&u=93aa7a0037a926090eb975aaf590840a069f6a44&s=140",
+      "websites": [
+        {
+          "name": "Lucca",
+          "url": "https://www.lucca.fr/"
+        }
+      ],
+      "location": "Nantes",
+      "sessions": [
+        {
+          "title": "16 ans de gestion produit, les hauts et les bas condensés en 20 minutes",
+          "abstract": "De la naissance du POC à la solution SaaS utilisée par 120 000 salariés, je vous raconterai les étapes clés en vous donnant quelques pistes sur les écueils à éviter. L’histoire vraie et toujours d’actualité d’un logiciel partagée par son Product Owner.",
+          "tags": [
+            "POC",
+            "Saas",
+            "scalabilité",
+            "RH",
+            "Product Owner"
+          ],
+          "lang": [
+            "fr"
+          ]
+        }
+      ],
+      "cities": [
+        "Nantes"
+      ],
+      "contacts": {
+        "twitter": "@fpot",
+        "mail": "fpot@lucca.fr"
+      }
     }
   ],
   "cities": {
