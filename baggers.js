00fd597bae56863121dfe8d6dd3aa59ed371d821
Guillaume Mouron <gmouron@lucca.fr>
2016-09-22 13:39:41 +0200

    Add Guillaume Mouron

--- baggers.js
+++ baggers.js
@@ -13512,6 +13512,61 @@ var data = {
         "twitter": "JoGrenat",
         "mail": "jordan.grenat+bbl@gmail.com"
       }
+    },
+    {
+      "since": "2016-09-22",
+      "name": "Guillaume Mouron",
+      "bio": "Ingénieur DevOps et Responsable de la production chez Lucca, éditeur de logiciels SaaS de gestion RH. Avant, ça j'ai travaillé 7 ans aux Pays-Bas à Keygene, entreprise de biotechnologies. Je suis full-stack mais avec une préférence pour le backend, notamment en C#.",
+      "picture": "https://avatars3.githubusercontent.com/u/950148?s=200",
+      "websites": [
+        {
+          "name": "Web",
+          "url": "https://www.lucca.fr/"
+        },
+        {
+          "name": "Web",
+          "url": "http://www.gmo-web.info/"
+        },
+        {
+          "name": "Web",
+          "url": "https://gmouron.github.io/"
+        },
+        {
+          "name": "LinkedIn",
+          "url": "https://fr.linkedin.com/in/gmouron"
+        },
+        {
+          "name": "GitHub",
+          "url": "https://github.com/GMouron"
+        }
+      ],
+      "location": "Lyon, Nantes",
+      "sessions": [
+        {
+          "title": "Construire un moteur de règles en C# en 1h chrono",
+          "abstract": "À travers une session de live coding, je vais vous montrer qu'il est possible de créer un petit moteur de règles en C# en moins d'1h grâce aux Expression Trees.",
+          "tags": [
+            "Live coding",
+            "C#",
+            "Expression trees",
+            "Arbres d'expression",
+            "Rule engine",
+            "Moteur de règles",
+            ".NET"
+          ],
+          "lang": [
+            "fr", "en"
+          ]
+        },
+      ],
+      "cities": [
+        "Lyon",
+        "Nantes"
+      ],
+      "contacts": {
+        "twitter": "GMouron",
+        "mail": "gmouron@lucca.fr"
+      }
     }
   ],
   "cities": {
