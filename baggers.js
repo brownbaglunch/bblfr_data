dbad706407bf9e996967ccf3354f8ce7e93f8681
Jean-Baptiste Beuzelin <jbbeuzelin@users.noreply.github.com>
2016-11-20 22:05:32 +0100

    ADD Jean-Baptiste Beuzelin (#153)
    * ADD Jean-Baptiste Beuzelin
    
    * Fix : Jean Baptiste profile
    
    * Fix : real fix

--- baggers.js
+++ baggers.js
@@ -13899,6 +13899,45 @@ var data = {
             "Paris",
             "Lyon"
         ]
+    },
+    {
+      "since": "2016-11-20",
+      "name": "Jean-Baptiste Beuzelin",
+      "bio": "Ingénieur R&D chez Lucca, je m'occupe de développer une solution interne de gestion de parc client et de facturation.",
+      "picture": "http://www.lucca.fr/img/equipe/jbeuzelin.png",
+      "websites": [
+        {
+          "name": "Web",
+          "url": "https://www.lucca.fr/"
+        },
+        {
+          "name": "GitHub",
+          "url": "https://github.com/jbbeuzelin"
+        }
+      ],
+      "location": "Paris, Nantes",
+      "sessions": [
+        {
+          "title": "Comment développer un outil qui permet de rendre vos collaborateurs plus efficaces.",
+          "abstract": "Vous souhaitez simplifier la vie de vos collaborateurs en automatisant leurs processus, en offrant de nouveaux outils afin qu’ils puissent rester autonome ?<br />Je vous offre alors mon retour d'expérience sur 3 ans de développement d'un outil de gestion de parc client et de facturation qui aide notre société Lucca à offrir les meilleurs services possibles.",
+          "tags": [
+            "Saas",
+            "SI",
+            "Tooling"
+          ],
+          "lang": [
+            "fr"
+          ]
+        }
+      ],
+      "cities": [
+        "Paris",
+        "Nantes"
+      ],
+      "contacts": {
+        "twitter": "JB_Beuzelin",
+        "mail": "jbbeuzelin@gmail.com"
+      }
     }
   ],
   "cities": {
