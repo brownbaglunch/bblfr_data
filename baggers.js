78aec2b9feb7749681a3e0cc1997329bbe44e923
Frédéric Camblor <fcamblor@gmail.com>
2017-01-16 15:41:02 +0100

    Adding fcamblor as a bagger

--- baggers.js
+++ baggers.js
@@ -14115,7 +14115,45 @@ var data = {
         "mail": "raphaelht@gmail.com",
         "twitter": "rap2h"
       }
-    }
+    }, {
+      "since": "2017-01-16",
+      "name": "Frédéric Camblor",
+      "bio": "Java & Frontend Web dev, Créateur de la conférence BDX I/O, Commiter RestX, travaille pour 4SH France",
+      "picture": "https://pbs.twimg.com/profile_images/378800000759015070/cb819fce4817052c11c7983fe932d210.png",
+      "websites": [
+        {
+          "name": "Web",
+          "url": "https://fcamblor.wordpress.com/"
+        },
+        {
+          "name": "GitHub",
+          "url": "https://github.com/fcamblor/"
+        }
+      ],
+      "location": "Bordeaux",
+      "sessions": [
+        {
+          "title": "TypeScript : dès lundi sur votre App Legacy",
+          "abstract": "Avez-vous déjà développé une <strong>App Front</strong> de 5K+ lignes de  JS ? Ne vous êtes-vous jamais senti perdu lorsque vous êtes revenus sur votre propre code sans connaître la structure et les méthodes des objets que vous manipuliez ?.. Et ne parlons même pas du code écrit par vos collègues !<br/><br/><strong>TypeScript (TS) et son typage statique</strong> peut vous aider à passer moins de temps à debugger dans votre browser en codant de manière <strong>type-safe</strong> !<br/>Et bonne nouvelle : vous n'avez pas besoin de démarrer un projet from scratch sous Angular 2 pour commencer à l'utiliser !<br/><br/>Ce talk sera <strong>100% livecoding</strong> et montrera une mise en place <strong>progressive de TypeScript</strong> sur <strong>un projet legacy écrit en JS</strong>.<br/><br/>Nous verrons :<br/><ul><li>Comment mettre en place la <strong>compilation TS</strong> en tâche de fond sur le projet, faire cohabiter les fichiers TS avec les fichiers JS ainsi que comprendre le code généré</li><li> L'interopérabilité avec les librairies JS non écrites en TS via l'outillage <strong>typings</strong></li><li> Enfin, nous migrerons du code JS en TS pour identifier les bienfaits apportés par le langage : types (classes/interfaces), modules, closures, immutabilité, paramètres optionnels, union types, complétion, doc implicite</li></ul>",
+          "tags": [
+            "Typescript",
+            "Typings",
+            "Transpiler",
+            "Languages"
+          ],
+          "lang": [
+            "fr"
+          ]
+        }
+      ],
+      "cities": [
+        "Bordeaux"
+      ],
+      "contacts": {
+        "twitter": "fcamblor",
+        "mail": "fcamblor@gmail.com"
+      }
+    }    
   ],
   "cities": {
     "Aix-en-Provence": {
