ee683c71844c26abbeefd904dd4da202414a1d16
SimplyGreenIT <ophilippot@greenspector.com>
2017-05-03 13:26:26 +0200

    Ajout de Olivier Philippot

--- baggers.js
+++ baggers.js
@@ -14794,7 +14794,68 @@ var data = {
             "lang": [ "fr" ]
           }
         ]
-     }    
+     },
+     {
+       "since": "2017-05-03",
+       "name": "Olivier Philippot",
+       "bio": "Consultant eco-conception logicielle; |CTO chez Greenspector",
+       "picture": "https://avatars0.githubusercontent.com/u/28344074?v=3&s=460",
+       "websites": [
+         {
+           "name": "Web",
+           "url": "https://greenspector.com/"
+         }
+       ],
+       "location": "Nantes",
+       "sessions": [
+         {
+           "title": "Comment être un développeur ethique et green ? Dilemne entre la course technologique et mon impact sur la société.",
+           "abstract": "Le logiciel est partout. Il est clair que le logiciel a un impact réel sur le monde... Nous, développeurs sommes les architectes du monde virtuel au service du réel. Nos actions ont un effet bien réel. Bénéfique mais aussi néfaste. Exclusion sociale, impact environnemental, obsolescence sont des effets bien réel de nos logiciels. Mais avons nous le choix face au demandes de nos utilisateurs et clients et aux contraintes associées (planning, sécurité...) Et bien oui, c'est le choix qui est fait par de nombreuses sociétés et développeurs : bénévolat comme code for America, Eco-conception de logiciel publique... Etre développeur ethique et green est possibles, nous verrons concretement comme faire cela au jour le jour.",
+           "tags": [
+             "Green",
+             "Eco-conception",
+             "Ethique"           ],
+           "lang": [
+             "fr"
+           ]
+         },
+         {
+           "title": "Iotception : Mesure de l'énergie d'Android Things sur Raspberry avec Arduino",
+           "abstract": "(introduction / live coding / présentation) Découvrez l'IoT avec cette session qui mèle hardware, DIY et logiciel avec l'angle green. Session jouée à Breizhcamp, Android Maker",
+           "tags": [
+             "IoT",
+             "Android Things",
+             "Green",
+             "Raspberry"
+           ],
+           "lang": [
+             "fr"
+           ]
+         },
+         {
+           "title": "Mesurez votre consommation d'énergie sur Android et sur HTML5",
+           "abstract": "L'autonomie est un critère important pour les utilisateurs mobiles mais aussi pour l'iOT. Mais elle est très rarement mesurée par les développeurs d'application ou même de site web ou d'application. On ne découvre des problèmes que trop tard (ou jamais). Les plateformes actuelles fournissent cependant des API permettant de récupérer des données. Android, HTML5... En 5 mn, vous pouvez avoir une donnée d'énergie. Je vous expliquerai comment utiliser ces API, comment utiliser et interpréter les données, et les utiliser dans vos tests.",
+           "tags": [
+             "Android",
+             "HTML5",
+             "Green",
+             "Energie"
+           ],
+           "lang": [
+             "fr"
+           ]
+         }
+       ],
+       "cities": [
+         "Paris",
+         "Nantes",
+         "Rennes"
+       ],
+       "contacts": {
+         "twitter": "simplygreenit",
+         "mail": "ophilippot@greenspector.com"
+       }
+     }
   ],
   "cities": {
     "Aix-en-Provence": {
