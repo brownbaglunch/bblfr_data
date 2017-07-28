c94e2184395809bc55ebe6a3a5af9be9b8bd8b41
Sylvain MONTEILLET <monteillet.s@mipih.fr>
2017-07-28 14:44:41 +0200

    Ajout d'un bagger

--- baggers.js
+++ baggers.js
@@ -15706,6 +15706,52 @@ var data = {
         "twitter": "sanpi_",
         "mail": "nicolas+bbl@joseph.pro"
       }
+    },
+    {
+      "since": "2017-07-28",
+      "name": "Sylvain Monteillet",
+      "bio": "Développeur agile",
+      "picture": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/1/005/076/26d/3830312.jpg",
+      "websites": [
+        {
+          "name": "GitHub",
+          "url": "https://github.com/smonteillet"
+        },
+        {
+          "name": "LinkedIn",
+          "url": "https://www.linkedin.com/in/sylvain-monteillet-52403aa0/"
+        }
+      ],
+      "location": "Toulouse",
+      "sessions": [
+        {
+          "title": "Collaborer avec Git",
+          "abstract": "Dans cette présentation, nous allons aborder des outils mis à disposition par git nous permettant de gérer nos commits ainsi que ceux des gens avec qui on collabore. Nous parlerons de merge, de rebase et de cherry-pick illustrés au travers de 2 modèles de branching: le trunk based development et feature branch.",
+          "tags": [
+            "Git"
+          ],
+          "lang": [
+            "fr"
+          ]
+        },
+        {
+          "title": "Réorganiser vos commits avec Git rebase interactive",
+          "abstract": "Dans cette démo, nous allons prendre un historique de commits locaux un peu bancal et le refaçonner pour lui donner tout son sens.",
+          "tags": [
+            "Git"
+          ],
+          "lang": [
+            "fr"
+          ]
+        }
+      ],
+      "cities": [
+        "Toulouse"
+      ],
+      "contacts": {
+        "twitter": "smonteillet",
+        "mail": "sylvain.monteillet@gmail.com"
+      }
     }
   ],
   "cities": {
