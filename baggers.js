e273d0a06b146726db465c4f844a46e738b16e16
François Sarradin <fsarradin@gmail.com>
2017-09-12 22:29:29 +0200

    Change tags for a presentation

--- baggers.js
+++ baggers.js
@@ -17,7 +17,6 @@ var data = {
           "title": "Ce que vous devez savoir de la programmation fonctionnelle",
           "abstract": "La programmation fonctionnelle est un paradigme qui est beaucoup plus ancien que vous ne le pensez et qui a eu des impacts assez importants dans le domaine du développement en général. Pourtant, ce n'est que très récemment que ce paradigme a commencé à émerger auprès des développeurs de tout bord. Je vous propose de découvrir ce qui définit la programmation fonctionnelle, les critiques qu'elle fait concernant le développement traditionnel et comment elle permet de répondre aux enjeux des projets les plus récents. Cette présentation s'adresse aussi bien à un public développeur ou non.",
           "tags": [
-            "Scala",
             "Programmation fonctionnelle"
           ],
           "lang": [
