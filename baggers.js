066b149d05714b4fbdc102d33c752a9f1ad144b2
kanedafromparis <kanedafromparis@gmail.com>
2017-09-22 12:05:45 +0200

    Update baggers.js

--- baggers.js
+++ baggers.js
@@ -15799,8 +15799,8 @@ var data = {
           ]
         },
         {
-          "title": "Une alternative à AOP ?",
-          "abstract": "Premiers pas avec Docker. Il s’agit dans un premier temps de présenter Docker, ses cas d’usage et quelques bonnes pratiques d’utilisation. Le but est de présenter Docker, son mode de fonctionnement et son écosystème. Ce qu’il peut apporter et les pièges à éviter",
+          "title": "Premiers pas avec Docker",
+          "abstract": "Il s’agit dans un premier temps de présenter Docker, ses cas d’usage et quelques bonnes pratiques d’utilisation. Le but est de présenter Docker, son mode de fonctionnement et son écosystème. Ce qu’il peut apporter et les pièges à éviter",
           "tags": [
             "Openshift",
             "kubernetes",
