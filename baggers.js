41b093b0d9d934ca396b9dc0cada71c1416a089e
Florian Beaufumé <fbeaufume@users.noreply.github.com>
2017-10-13 12:57:36 +0200

    Added profile of Florian Beaufumé

--- baggers.js
+++ baggers.js
@@ -15887,6 +15887,72 @@ var data = {
         "twitter": "romaintouze",
         "mail": "contact@rtodev.tech"
       }
+    },
+    {
+      "since": "2017-10-13",
+      "name": "Florian Beaufumé",
+      "bio" : "Architecte logiciel, expert Java, freelance",
+      "picture" : "https://avatars2.githubusercontent.com/u/25341695",
+      "cities" : [
+        "Toulouse"
+      ],
+      "contacts" : {
+        "twitter": "fbeaufume",
+        "mail": "fbeaufume@gmail.com"
+      },
+      "sessions": [
+        {
+          "title": "Programmation concurrente en Java",
+          "abstract": "Présentation du modèle de concurrence de Java ainsi que de ses API principales: Thread, volatile, Executor, fork/join, StampedLock, CompletableFuture et autres n'auront plus de secrets",
+          "tags": [
+            "Java",
+            "Concurrence"
+          ],
+          "lang": [
+            "fr"
+          ]
+        },
+        {
+          "title": "Programmation réactive avec Spring 5 et Reactor",
+          "abstract": "Pour supporter une charge élevée ou améliorer la robustesse de son backend, la programmation réactive peut aider. Nous allons la présenter et montrer sa mise en oeuvre concrête avec Spring 5 et la librairie Reactor",
+          "tags": [
+            "Programmation réactive",
+            "Spring",
+            "Reactor",
+            "Java"
+          ],
+          "lang": [
+            "fr"
+          ]
+        },
+        {
+          "title": "WebSocket avec Java EE 7",
+          "abstract": "Présentation de WebSocket, protocole de communication orienté temps réel pour le web, ainsi que son API dans Java EE 7, avec retour d'expérience de développement d'une application",
+          "tags": [
+            "WebSocket",
+            "Java EE",
+            "Java"
+          ],
+          "lang": [
+            "fr"
+          ]
+        }
+      ],
+      "location": "Toulouse et proximité",
+      "websites": [
+        {
+          "name": "Web",
+          "url": "http://www.adeliosys.fr/"
+        },
+        {
+          "name": "SlideShare",
+          "url": "https://www.slideshare.net/fbeaufume/presentations"
+        },
+        {
+          "name": "LinkedIn",
+          "url": "https://www.linkedin.com/in/fbeaufume/"
+        }
+      ]
     }
   ],
   "cities": {
