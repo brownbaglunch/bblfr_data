83fd5d8a5ec1becfc0a6f0911ad32e6592d256ab
Vincent Massol <vincent@massol.net>
2017-10-30 14:30:08 +0100

    Added Vincent Massol (#245)

--- baggers.js
+++ baggers.js
@@ -16042,6 +16042,55 @@ var data = {
           "url": "https://blog.ippon.fr/author/rkoci"
         }
       ]
+    },
+    {
+      "since": "2017-10-30",
+      "name": "Vincent Massol",
+      "bio" : "Développeur Open source jour et nuit, payé par XWiki SAS pour travailler sur le logiciel éponyme... Aime l'ingénierie logicielle et notamment le build, tests, et qualité de code.",
+      "picture" : "https://gravatar.com/avatar/0b17060fd5b56172e81e4bfbbcb438e0?&s=128",
+      "cities" : [
+        "Paris"
+      ],
+      "contacts" : {
+        "twitter": "vmassol",
+        "mail": "vincent@massol.net"
+      },
+      "sessions": [
+        {
+          "title": "Creating your own project's quality dashboard",
+          "abstract": "Live demo of using XWiki to create your own custom quality dashboard to aggregate various metrics from different sites (Jenkins, SonarQube, JIRA, GitHub). Store the metrics locally to draw history graphs of the metrics and implement some custom actions, for example send some alert email when some combined metric thresholds have been crossed.",
+          "tags": [
+            "Quality",
+            "Metrics",
+            "Dashboard",
+            "Jenkins",
+            "SonarQube",
+            "JIRA",
+            "GitHub",
+            "XWiki",
+            "Web Application"
+          ],
+          "lang": [
+            "fr",
+            "en"
+          ]
+        }
+      ],
+      "location": "Paris, Lamorlaye, Chantilly, Gouvieux, Val d'Oise",
+      "websites": [
+        {
+          "name": "Web",
+          "url": "http://massol.net"
+        },
+        {
+          "name": "AboutMe",
+          "url": "https://about.me/vmassol"
+        },
+        {
+          "name": "LinkedIn",
+          "url": "https://www.linkedin.com/in/vmassol/"
+        }
+      ]
     }
   ],
   "cities": {
