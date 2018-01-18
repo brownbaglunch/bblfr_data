f746304e005d79019bc6640da538a7182adaea4e
David Gageot <david@gageot.net>
2018-01-18 19:26:19 +0100

    back to BBLs (#264)

--- baggers.js
+++ baggers.js
@@ -1,6 +1,47 @@
 var data = {
   "speakers": [
     {
+      "since": "2012-11-05",
+      "name": "David Gageot",
+      "bio": "Developer Advocate pour la Google Cloud Platform. Développeur passionné!",
+      "picture": "http://gravatar.com/avatar/f0887bf6175ba40dca795eb37883a8cf?s=200",
+      "websites": [
+        {
+          "name": "Github",
+          "url": "https://github.com/dgageot"
+        },
+        {
+          "name": "Blog",
+          "url": "https://blog.javabien.net/"
+        }
+      ],
+      "location": "Paris",
+      "sessions": [
+        {
+          "title": "Transform your Legacy Application with Kubernetes and Istio",
+          "abstract": "Kubernetes is often used to run micro-services based applications. Istio's service mesh adds even more power and flexibility to those applications. But what about your Good Old Monolithic Legacy Application? Should you feel left alone if you're not cloud-native? I'm going to demonstrate how to leverage Kubernetes and Istio to enrich an existing application without touching a single line of its code. Because, you know, the original developers are long gone. Through lots of demos and zero slide, the attendees will learn to add Authentication, SSL Termination, Monitoring, Blue/Green Deployments, Canary builds, Fault tolerance...",
+          "tags": [
+            "Docker",
+            "Kubernetes",
+            "Istio",
+            "DevOps",
+            "Cloud"
+          ],
+          "lang": [
+            "fr",
+            "en"
+          ]
+        }
+      ],
+      "cities": [
+        "Paris"
+      ],
+      "contacts": {
+        "twitter": "dgageot",
+        "mail": "david@gageot.net"
+      }
+    },
+    {
       "since": "2013-02-09",
       "name": "François Sarradin",
       "bio": "Consultant Java/Scala/&#955; | Manager technique capitalisation chez Ippon Technologies",
