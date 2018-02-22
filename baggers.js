7e927260c6979c6b3841fb4e4be654a270e2c46e
mab <mabhub@users.noreply.github.com>
2018-02-22 15:11:58 +0100

    Create bagger profile for 'Benjamin Marguin' (me) (#272)
    * Create bagger profile entry for 'Benjamin Marguin'
    
    * Add a session about Git commits
    
    	Add the first session about Git in profile of 'Benjamin Marguin'
    
    * Add a session about Git usages
    
    	Add another session about Git in profile of 'Benjamin Marguin'
    
    * Add a session about signing commits and tags in Git
    
    	Add session in profile of 'Benjamin Marguin'

--- baggers.js
+++ baggers.js
@@ -14056,6 +14056,48 @@ var data = {
         mail: 'ronan@pocketsensei.net',
       },
     },
+    {
+      since: '2018-02-22',
+      name: 'Benjamin Marguin',
+      bio: 'Développeur front-end, intégrateur, formateur Git.',
+      picture: 'https://avatars3.githubusercontent.com/u/1846633',
+      location: 'Toulouse',
+      cities: ['Toulouse'],
+      contacts: {
+        twitter: 'mab_',
+        mail: 'brownbaglunch.fr@ma-b.com',
+      },
+      websites: [
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/benjamin-marguin',
+        },
+      ],
+      sessions: [
+        {
+          title: 'Qu’est-ce qu’un bon commit ?',
+          abstract:
+            'Vous utilisez Git seul ou en équipe, en débutant ou déjà depuis un bon moment, mais vous vous dites que vos commits pourraient être "mieux" (message, description, contenu, liens avec les forges) ? Parlons-en !',
+          tags: ['Git'],
+          lang: ['fr'],
+        },
+        {
+          title: 'Git au quotidien',
+          abstract:
+            'Ni une présentation ni une conférence, mais une proposition de prendre un peu de recul et de discuter ensemble de votre utilisation de Git et de vos éventuelles problématiques.',
+          tags: ['Git', 'Gitlab', 'Github'],
+          lang: ['fr'],
+        },
+        {
+          title:
+            'Signer numériquement ses commits ou ses tags, pourquoi et comment ?',
+          abstract:
+            'Au sein d’un dépôt Git, l’identification des auteurs n’est qu’indicative. Les données de base d’un commit ne permettent pas d’authentifier son auteur. Git offre néanmoins la possibilité, grâce aux signatures GPG d’ajouter cette information fiable aux commits et aux tags. Pourquoi faire, comment faire ? Si vous voulez qu’on en parle, faites moi signe !',
+          tags: ['Git', 'GPG', 'Chiffrement'],
+          lang: ['fr'],
+        },
+      ],
+    },
   ],
   cities: {
     'Aix-en-Provence': {
