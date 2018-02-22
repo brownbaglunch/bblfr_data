e3c6d27159af890d9bbb6661176a74eae384faa6
mab <git@ma-b.com>
2018-02-22 12:31:43 +0100

    Add a session about signing commits and tags in Git
    	Add session in profile of 'Benjamin Marguin'

--- baggers.js
+++ baggers.js
@@ -14088,6 +14088,14 @@ var data = {
           tags: ['Git', 'Gitlab', 'Github'],
           lang: ['fr'],
         },
+        {
+          title:
+            'Signer numériquement ses commits ou ses tags, pourquoi et comment ?',
+          abstract:
+            'Au sein d’un dépôt Git, l’identification des auteurs n’est qu’indicative. Les données de base d’un commit ne permettent pas d’authentifier son auteur. Git offre néanmoins la possibilité, grâce aux signatures GPG d’ajouter cette information fiable aux commits et aux tags. Pourquoi faire, comment faire ? Si vous voulez qu’on en parle, faites moi signe !',
+          tags: ['Git', 'GPG', 'Chiffrement'],
+          lang: ['fr'],
+        },
       ],
     },
   ],
