f9e077fe2e4ccc1422230a772f8d7182e563bc8a
Florian FERBACH <florian@marmelab.com>
2018-03-15 17:20:44 +0100

    Add Florian Ferbach

--- baggers.js
+++ baggers.js
@@ -14042,6 +14042,49 @@ var data = {
         },
       ],
     },
+    {
+      since: '2018-03-15',
+      name: 'Florian Ferbach',
+      bio:
+        "Facilitateur chez marmelab, co-organisateur des apéros agiles Nancy, enseignant à l'IUT Charlemagne, sidepreneur à mes heures perdues. Mon métier ? Je “pimp” la collaboration humaine et “tune” l’innovation à l’aide de l’agilité et du lean startup pour que les produits qu’on construit délivrent le maximum de valeur.",
+      picture:
+        'https://s.gravatar.com/avatar/81430cde0877c0c602e7af07a2df1924?s=150',
+      location: 'Nancy',
+      cities: ['Nancy'],
+      contacts: {
+        twitter: 'fferbach',
+        mail: 'florian.ferbach@gmail.com',
+      },
+      websites: [
+        {
+          name: 'Web',
+          url: 'https://www.marmelab.com/blog',
+        },
+      ],
+      sessions: [
+        {
+          title: 'Initiation aux valeurs agiles',
+          abstract:
+            "Qu'est-ce que l'agilité ? Mais surtout... qu'est-ce que n'est pas l'agilité ? A quoi ça sert et pourquoi ça fonctionne ?",
+          tags: ['Agilité'],
+          lang: ['fr'],
+        },
+        {
+          title: 'Initiation au Lean Startup',
+          abstract:
+            "Le lean startup n'est pas utile qu'aux startups mais à toute société cherchant à innover dans un contexte très incertain. Quelles sont les bases et pourquoi ça fonctionne ?",
+          tags: ['Lean Startup'],
+          lang: ['fr'],
+        },
+        {
+          title: "Scrum c'est quoi ?",
+          abstract:
+            "Quelles sont les bases de Scrum et pourquoi c'est déjà un bon départ ?",
+          tags: ['Agilité', 'Scrum'],
+          lang: ['fr'],
+        },
+      ],
+    },
   ],
   cities: {
     'Aix-en-Provence': {
