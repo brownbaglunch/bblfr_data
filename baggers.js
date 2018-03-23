c817220fe7722a8bc1012681994573b935b78929
Pierre Laporte <pierre@pingtimeout.fr>
2018-03-23 11:26:08 +0100

    Add Marine Corbelin

--- baggers.js
+++ baggers.js
@@ -14205,6 +14205,54 @@ var data = {
         mail: 'e.coutaud@gmail.com',
       },
     },
+    {
+     since: '2018-03-23',
+     name: 'Marine Corbelin Laporte',
+     bio: "Chef de projet 3D/VR/AR freelance. Je suis spécialisée dans la gestion de projet en réalité virtuelle et augmentée. Que vous souhaitiez simplement découvrir les usages de ces technologies ou approfondir l'aspect technique, je suis là pour vous accompagner.",
+     picture: 'https://pbs.twimg.com/profile_images/851152816429203456/cNAdBzYr_400x400.jpg',
+     websites: [
+       {
+         name: 'Web',
+         url: 'http://teapotmc.fr/',
+       },
+       {
+         name: 'LinkedIn',
+         url: 'https://www.linkedin.com/in/marine-corbelin-a03bb023/'
+       },
+     ],
+     location: 'Lille',
+     sessions: [
+       {
+         title: 'Tour d’horizon des usages de la VR et de l’AR',
+         abstract: "La VR et l'AR sont aujourd'hui en pleine explosion. Quels sont les usages de ces technologies? Comment vont-elles impacter notre futur?",
+         tags: ['VR', 'AR'],
+         lang: ['fr']
+       },
+       {
+         title: 'Comment gérer un projet VR/AR/ vidéo 360 ?',
+         abstract: "Vous vous lancez dans un projet VR/ AR ou vidéo 360°: Voici les bonnes pratiques pour commencer du bon pied.",
+         tags: ['3D', 'VR', 'AR'],
+         lang: ['fr']
+       },
+       {
+         title: 'La VR dans Unity/ Unreal Engine 4',
+         abstract: "Quels sont les outils disponibles dans Unity et Unreal pour créer un projet VR",
+         tags: ['VR', 'Unity', 'Unreal'],
+         lang: ['fr']
+       },
+       {
+         title: 'Les contraintes de la modélisation 3D pour du temps réel',
+         abstract: "Les bonnes pratiques et les outils pour créer du contenu pour vos applications VR et AR",
+         tags: ['3D', 'VR', 'AR'],
+         lang: ['fr']
+       },
+     ],
+     cities: ['Lille'],
+     contacts: {
+       twitter: 'nanakola',
+       mail: 'marine.teapotmc@gmail.com',
+     },
+    },
   ],
   cities: {
     'Aix-en-Provence': {
