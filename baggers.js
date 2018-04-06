8cb4972837a928179ef5590adfca3622621bfa45
Philippe Bourgau <philippe.bourgau@murex.com>
2018-04-06 18:00:37 +0200

    Add Philippe Bourgau Speaker
    - Add speaker
    - add one session

--- baggers.js
+++ baggers.js
@@ -14312,6 +14312,47 @@ var data = {
         },
       ],
     },
+    {
+      since: '2018-04-06',
+      name: 'Philippe Bourgau',
+      bio: 'Coach XP à Murex. J\'aide les équipes de dev à devenir durablement productives, en adaptant eXtreme Programming à leur context',
+      picture:
+      'http://www.gravatar.com/avatar/686399bd630ecc9381e4dfec8720816e?s=250',
+      websites: [
+        {
+          name: 'Web',
+          url: 'http://philippe.bourgau.net/',
+        },
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/philippe-bourgau-607a928/',
+        },
+        {
+          name: 'AboutMe',
+          url: 'http://philippe.bourgau.net/about-me/',
+        },
+        {
+          name: 'GitHub',
+          url: 'https://github.com/philou',
+        },
+      ],
+      location: 'Paris',
+      sessions: [
+        {
+          title: 'L’alchimie du BDD : changer votre code de plomb en or. Ou comment livrer de plus en plus de valeur.',
+          abstract:
+          "Vous êtes un véritable craftsman ! Tout se passe bien jusqu'au jour où, au détour d'une conversation, vous comprenez quelque chose de plus profond sur votre domaine métier ... Comment BDD débloque ces ‘refactoring breakthrough’ ? Comment transformer cette opportunité ? Qu’y gagne t’on exactement ?",
+          tags: ['BDD', 'DDD', 'Refactoring'],
+          lang: ['fr', 'en'],
+        },
+      ],
+      cities: ['Paris'],
+      contacts: {
+        twitter: 'pbourgau',
+        mail: 'philippe.bourgau@gmail.com',
+      },
+    },
+
 
   ],
   cities: {
