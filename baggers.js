47a1e1a8b4d8e9433bafd08c2af9791b3d9777c9
Alexis Janvier <contact@alexisjanvier.net>
2018-05-21 10:40:27 +0200

    Add abstracts

--- baggers.js
+++ baggers.js
@@ -14632,31 +14632,36 @@ var data = {
       sessions: [
         {
           title: 'Le télétravail',
-          abstract: 'A FAIRE',
+          abstract:
+            'Cela fait maintenant deux ans que je suis en télétravail à plein temps, me permettant d’en faire un bilan : quels sont les problèmes logistiques, comment réussir l’organisation de ses journées et trouver le difficile équilibre entre vie personnelle et vie professionnelle. Se sent-on seul ? Netflix est-il une drogue ? Si votre entreprise envisage de mettre en place du télétravail, je suis disponible pour venir vous parler de ce bilan, ou tout simplement tenter de répondre aux questions que vous vous posez sur ce mode d’organisation.',
           tags: ['remote', 'teletravail', 'agile', 'team'],
           lang: ['fr'],
         },
         {
           title: 'Découverte de docker',
-          abstract: 'A FAIRE',
+          abstract:
+            'Je rencontre souvent des développeurs souhaitant utiliser Docker, mais sans trop savoir finalement ce que c’est ni par quel bout commencer à l’utiliser. Je vous propose de vous faire découvrir les bases de Dockers et voir comment intégrer dans votre worflow de développement cet outil que j’utilise maintenant au quotidien.',
           tags: ['docker', 'docker-compose', 'devops'],
           lang: ['fr'],
         },
         {
           title: 'Lancer et maintenir un projet open-source',
-          abstract: 'A FAIRE',
+          abstract:
+            'L’open-source est selon moi l’une des grandes idées du siècle ! Sans être un grand contributeur, j’ai eu l’occasion de maintenir des petits projets, mais aussi de contribuer à des idées plus ambitieuses. Je vous propose donc de venir vous présenter les bonnes bases permettant de lancer un projet open-source, et de voir les avantages et les contraintes que cela peu induire.',
           tags: ['open-source'],
           lang: ['fr'],
         },
         {
           title: 'Les tests en Javascript',
-          abstract: 'A FAIRE',
+          abstract:
+            'Si « Le bon sens est la chose la mieux partagée », l’utilisation des tests n’est pas toujours la chose la mieux partagée par tous les développeurs. Et pourtant, ne pas les utiliser, c’est se passer d’un outil fondamental dans la production d’un code bien écrit et pérenne, le genre de code permettant d’être serein avec sa production. Je vous propose donc de vous présenter les briques de bases permettant de mettre en place des tests sur un projet en javascript (tests unitaires, fonctionnelles, end-to-end, intégration continue, …).',
           tags: ['javascript', 'tests', 'jest', 'cypress'],
           lang: ['fr'],
         },
         {
           title: 'La revue de code',
-          abstract: 'A FAIRE',
+          abstract:
+            'Après presque 15 ans de développement, j’affirme que la revue de code est la ciment d’une équipe de développement. Sans code review, rien de solide ne peut être réellement contruit. Je vous propose donc de venir vous présenter les bienfaits de la mise en place d’une revue de code systèmatique, mais aussi les méfaits d’une revue de code malveillante.',
           tags: ['git', 'github', 'team'],
           lang: ['fr'],
         },
