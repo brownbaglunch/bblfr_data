916452494a9b037a393725f6b9e58e63917ced7d
Julien Pauli <jpauli@php.net>
2018-06-01 15:13:47 +0200

    Add new speaker

--- baggers.js
+++ baggers.js
@@ -14672,6 +14672,46 @@ var data = {
         mail: 'contact@alexisjanvier.net',
       },
     },
+    {
+      since: '2018-06-01',
+      name: 'Julien Pauli',
+      bio:
+        'System and Web architect at SensioLabs',
+      picture:
+        'https://avatars2.githubusercontent.com/u/72415?s=460&v=4',
+      websites: [
+        {
+          name: 'Github',
+          url: 'https://github.com/jpauli',
+        },
+        {
+          name: 'Blog',
+          url: 'http://blog.jpauli.tech/',
+        },
+      ],
+      location: 'Paris',
+      sessions: [
+        {
+          title: 'Tuez votre Box – Reprenez le contrôle de votre réseau, chez vous',
+          abstract:
+            "Cette conférence va montrer comment monter chez soi une infra digne d’une petite entreprise. Quelques idées : Infra dual-stack IPV4/IPV6 puis, VLANs pour isoler son traffic (IOT notamment), règles de firewall poussées, mais aussi création d’un Internet en VPN, routage dynamique avec BGP/OSPF, multi homing (multiples FAI) avec équilibrage de charge, et bien plus encore. Reprenez le contrôle en gardant vos données chez vous et en montant votre infra en mode DIY. En théorie, vous devriez être motivé à jeter votre Box Internet à la fin de la présentation.",
+          tags: ['Boxes', 'Router', 'DIY', 'Mikrotik', 'IPV6'],
+          lang: ['fr'],
+        },
+        {
+          title: 'Les fondamentaux de la cryptographie',
+          abstract:
+            "Cette présentation va montrer la base commune derrière tout processus cryptographique informatique. Nous parlerons du chiffrement de Vernam, appliqué dans la machine Enigma à l'époque , pour l'appliquer au domaine de l'informatique. Nous verrons une implémentation précise de la notion de chiffrement : le chiffrement par flot et les registres à décalage. Ces structures sont derrière la notion d'aléatoire en informatique. Le but est que tout le monde comprenne bien techniquement, comment fonctionne la base de tous les algorithmes de cryptographie du monde, sans pour autant entrer dans des formules mathématiques incompréhensibles. Nous coderons une machine de chiffrement par flot, pas à pas, en PHP puis sous forme d'extension PHP (en C). Les (vieux) algorithmes RC4 et A51 seront analysés puis implémentés pas à pas à titre d'exemple dans des classes PHP.",
+          tags: ['cryptographie', 'xor', 'php', 'vernam', 'lfsr'],
+          lang: ['fr'],
+        },
+      ],
+      cities: ['Paris'],
+      contacts: {
+        twitter: 'julienpauli',
+        mail: 'julien.pauli@gmail.com',
+      },
+    },
   ],
   cities: {
     'Aix-en-Provence': {
