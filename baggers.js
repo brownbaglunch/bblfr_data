df09664df1ff1dc28354c39811956e1d25f0ea6c
Cyril Lakech <40664264+cyril-lakech@users.noreply.github.com>
2018-09-21 22:13:30 +0200

    feat: add NestJS talks (#310)

--- baggers.js
+++ baggers.js
@@ -5198,6 +5198,13 @@ var data = {
           tags: ['Développeur', 'Technologie', 'Radar', 'Communauté'],
           lang: ['fr'],
         },
+        {
+          title: 'Un zeste de Nest pour réhausser le goût du back-end JS',
+          abstract:
+            'Nous connaissons tous un développeur frontend qui est devenu plus efficace grâce à des solutions comme Angular, Vue ou React. On connait également des développeurs backend qui développent de manière architecturée grâce à Spring Boot pour Java ou ASP.NET Core pour .NET Core. Et côté NodeJS ? 99% utilisent Express, qui est loin d’être un équivalent… Pourtant il existe NestJS qui propose des facilitant pour créer des APIs REST ou GraphQL au top! (avec @lauthieb)',
+          tags: ['NodeJS', 'TypeScript', 'Angular', 'API'],
+          lang: ['fr'],
+        },
       ],
       cities: ['Lille'],
       contacts: {
