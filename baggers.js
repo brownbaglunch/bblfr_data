2c80a53aefb62069efa964528f9ec0be89d0e259
Julien Jakubowski <julien@jakubowski.fr>
2019-02-24 20:41:55 +0100

    added jak78 Epic Legacy Code Challenge

--- baggers.js
+++ baggers.js
@@ -761,6 +761,19 @@ var data = {
           lang: ['fr'],
         },
         {
+          title: 'Epic Legacy Code Challenge',
+          abstract:
+            "Vous venez d'être embauchés chez Paladin Inventories Company. Elle gagne beaucoup d'argent avec son logiciel. Mais le gobelin qui l'a écrit s'est fait dévorer par un dragon. Vous ne comprenez rien au code. Et bien sûr il n'y a pas de tests. Le problème : une horde de trolls se rue vers vous avec plein de nouvelles demandes pour ce logiciel, qui est maintenant le vôtre...\nCette situation vous dit quelquechose ?\nEt si on se mettait dans cette situation, en équipes et en compétition ? Qui sera alors l'équipe la plus à même de faire évoluer le code legacy, tout en continuant à gagner de l'argent ?\nVenez relever le challenge, avec votre env de dev favori et Node.js installé au préalable !",
+          tags: [
+            'Test',
+            'Dojo',
+            'Software Craftsmanship',
+            'Serious game',
+            'Fun',
+          ],
+          lang: ['fr'],
+        },
+        {
           title: 'Alliés contre les défauts (talk avec le WebCenter AXA)',
           abstract:
             'La pratique de la revue de code prévient énormément de défauts. Mais elle a aussi d’autres “effets kiss-cool”. C’est une pratique qui existe depuis une soixantaine d’années. Mais pourquoi est-elle si rarement rencontrée dans les DSI ? Sans doute parce qu’elle est méconnue, mais aussi parce qu’elle fait l’objet d’appréhensions. Faites-vous des revues de code chez vous ? Et sinon, pourquoi pas ? Peut-être pensez-vous que cela prend trop de temps, que c’est trop cher, qu’en jugeant le code on jugera l’auteur… Au Webcenter AXA, le code est revu systématiquement avant de partir en production. Cela porte ses fruits. Mais cela ne s’est pas fait sans accrocs. Venez découvrir un retour d’expérience.',
