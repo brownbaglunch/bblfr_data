b700c8bf93c35322882aec6e37130ed9c902a2aa
caedes <laurentromain@gmail.com>
2019-04-16 23:39:23 +0200

    feat(baggers): add new bagger from Bordeaux

--- baggers.js
+++ baggers.js
@@ -16127,6 +16127,60 @@ var data = {
         mail: 'hello@maneu.net',
       },
     },
+    {
+      since: '2019-04-16',
+      name: 'Romain Laurent',
+      bio:
+        'Architecte Web, formateur JavaScript/Node/React, développeur full stack et nouveau software craftsman',
+      picture:
+        'https://secure.gravatar.com/avatar/5aaaecf4c870c833121f034df92b7586?s=400',
+      websites: [
+        {
+          name: 'Github',
+          url: 'https://github.com/caedes',
+        },
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/romainlaurent/',
+        },
+      ],
+      location: 'Bordeaux',
+      sessions: [
+        {
+          title: 'Introduction à React',
+          abstract:
+            'Créer des composants web simples rapidement, Créer une première Single Page App avec ReactJS, Appréhender CodeSandbox à travers un TP orienté e-commerce.',
+          tags: ['React', 'JavaScript'],
+          lang: ['fr'],
+        },
+        {
+          title: 'Introduction à Redux',
+          abstract:
+            "Mise en place de Redux au sein d'une application React existante.",
+          tags: ['React', 'Redux', 'JavaScript'],
+          lang: ['fr'],
+        },
+        {
+          title: 'Scalable Folder Structure in a React/Redux Application',
+          abstract:
+            'Je serai prêt à animer cette session à partir de juin 2019.',
+          tags: ['React', 'JavaScript', 'Redux', 'DDD'],
+          lang: ['fr'],
+        },
+        {
+          title:
+            'Comment animer un entretien technique front ? Comment obtenir le plein potentiel des développeurs ?',
+          abstract:
+            "Cette session tend à représenter l'ensemble des questions types lors d'un entretien d'embauche front React. Il reflète quelques années d'expérience dans ce domaine, avec ses succès et ses échecs.",
+          tags: ['Développeur', 'Recrutement', 'Front', 'React', 'JavaScript'],
+          lang: ['fr'],
+        },
+      ],
+      cities: ['Bordeaux'],
+      contacts: {
+        twitter: 'caedes',
+      },
+    },
   ],
   cities: {
     'Aix-en-Provence': {
