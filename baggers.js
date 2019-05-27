a14431d1fadba5b66412ac1a96fe025118b33408
Marthym <marthym@framagit.com>
2019-05-27 20:32:25 +0200

    fix(baggers): Update git session name for fcombes

--- baggers.js
+++ baggers.js
@@ -16316,7 +16316,7 @@ var data = {
       ],
       sessions: [
         {
-          title: 'Git au quotidien',
+          title: 'Git c’est facile !',
           abstract:
             'Principal outil du développeur, git est souvent utilisé mais pas toujours maitrisé. Que se passe t’il derrière les commandes que l’on utilise tous les jours ? Nous verrons les concepts de base de Git qui permettrons de comprendre ce qu’il se passe sous le capot et les astuces qui optimiseront votre quotidien de développeur.',
           tags: ['git', 'dev', 'version'],
