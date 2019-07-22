8dc69fa661e8a519e54246e9ec613f49b72fc485
Nicolas Torion <nicolas.torion@bearstudio.fr>
2019-07-22 14:06:12 +0200

    Add to speakers ntorion

--- baggers.js
+++ baggers.js
@@ -16416,6 +16416,34 @@ var data = {
       },
     },
     {
+      since: '2019-07-22',
+      name: 'Nicolas Torion',
+      bio:
+        "Développeur Full-Stack @ BearStudio",
+      picture: 'https://pbs.twimg.com/profile_images/1102599280513220608/Dgncv0EI_400x400.png',
+      websites: [
+          { name: 'Twitter', url: 'https://twitter.com/NicoTotor' },
+      ],
+      sessions: [
+        {
+          title: 'REACT NATIVE : REACT POUR DÉVELOPPER DES APPLICATIONS NATIVES ANDROID ET IOS',
+          abstract: 'React s’est imposé depuis plusieurs années comme une référence dans le développement d’applications web dynamiques. Après le web, le projet Open-Source s’est étendu en 2015 au mobile avec React Native et avec pour objectif de simplifier le développement d’applications mobiles natives Android et iOS et de permettre aux développeurs d’avoir un unique projet fonctionnel sur les deux plateformes. Au BearStudio, nos clients ont souvent pour besoin de lancer une première version de leur produit sur le marché avec de fortes contraintes de temps et de budget. Dans ce contexte, React Native a rapidement trouvé sa place dans nos solutions technologiques pour répondre à ces besoins. Et nous avons ainsi pu développer 4 applications mobiles en l\'espace d\'une année avec cette technologie. Ce talk sera donc un retour d\'expérience sur React Native et présentera tout ce qu\'il faut connaitre pour démarrer un projet React Native :',
+          tags: [
+            'React Native',
+            'Android',
+            'iOS',
+          ],
+          lang: ['fr'],
+        },
+      ],
+      location: 'Rouen',
+      cities: ['Rouen'],
+      contacts: {
+        twitter: 'NicoTotor',
+        mail: 'nicolas.torion@bearstudio.fr',
+      },
+    },
+    {
       since: '2015-03-10',
       name: 'Xavier Pigeon',
       bio:
