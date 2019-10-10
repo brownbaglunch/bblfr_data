f5abfb622d17291529d5acd03dc885329a2eebdf
Philippe Chevreul <32156946+SoulAjnin@users.noreply.github.com>
2019-10-10 10:47:56 +0200

    add philippe chevreul (#396)

--- baggers.js
+++ baggers.js
@@ -16416,6 +16416,32 @@ var data = {
       },
     },
     {
+      since: '2019-07-22',
+      name: 'Philippe Chevreul',
+      bio:
+        "UX/UI Designer - Développeur Front @ BearStudio -- Designer et Developpeur en devenir, j'aime concevoir et créer des interfaces fonctionnelles pour des utilisateurs !",
+      picture: 'https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/17760227_1613741305320428_2908477432045572687_n.jpg?_nc_cat=103&_nc_oc=AQlZtWOA4xRvETEVbOo8IfB8Ty9WRgnBkCRuB4R6WfHKfxSQHiRid2pukY4b1aT_-X4&_nc_ht=scontent-cdt1-1.xx&oh=66f48ff05b9673e63ec4da68477dddc2&oe=5DABB05E',
+      websites: [
+        { name: 'Web', url: 'https://philippechevreul.fr' },
+        { name: 'LinkedIn', url: 'https://www.linkedin.com/in/philippe-chevreul-64302212b/' },
+      ],
+      location: 'Rouen',
+      sessions: [
+        {
+          title: 'Le bon sens au service de vos utilisateurs',
+          abstract:
+            'Dans cette session, nous verrons comment mettre l\'utilisateur au centre de votre solution, avec quelques règles de bon sens pour vos interfaces',
+          tags: ['ergonomie', 'ux', 'ui', 'web'],
+          lang: ['fr'],
+        },
+      ],
+      cities: ['Rouen'],
+      contacts: {
+        twitter: 'philcvrl',
+        mail: 'tic@bearstudio.fr',
+      },
+    },
+    {
       since: '2015-03-10',
       name: 'Xavier Pigeon',
       bio:
