40e1f1fb989384a137a0fc82351eded907000e00
Romain Linsolas <romain.linsolas@sgcib.com>
2019-10-10 14:19:08 +0200

    fix: correct typo

--- baggers.js
+++ baggers.js
@@ -15066,7 +15066,7 @@ var data = {
         },
         {
           title:
-            'Sprint 0 : et si on s'en passait ?',
+            'Sprint 0 : et si on s\'en passait ?',
           abstract:
             "Que ce soit le design graphique, le design technique (architecture), la conception émergente est une des clés de l'agilité. Préparer des maquettes, un socle technique ou faire une phase de cadrage technique est souvent une preuve de manque d'agilité, alors, on fait comment pour des produits très complexe ?",
           tags: ['Agile', 'Scrum', 'Produit', 'Valeur', 'Priorisation', 'Architecture', 'Sprint 0', 'Cadrage'],
