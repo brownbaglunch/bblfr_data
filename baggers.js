99b7bd9f6a34041560d66d0f240fbb6460a0a236
Ilan Zerath <ilan.prims@gmail.com>
2019-10-28 20:37:26 +0100

    feat(baggers) - Add Ilan Zerath as new Baggers

--- baggers.js
+++ baggers.js
@@ -16933,6 +16933,48 @@ var data = {
         mail: 'rudy@bearstudio.fr',
       },
     },
+    {
+      since: '2019-10-28',
+      name: 'Ilan Zerath',
+      bio:
+        'Craftsman - Full Stack Engineer @ beNext.',
+      picture:
+        'https://pbs.twimg.com/profile_images/1081844752084017152/sCswQBbg_400x400.jpg',
+      websites: [
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/ilan-zerath-50752371/',
+        },
+      ],
+      location: 'Paris et alentours',
+      sessions: [
+        {
+          title: 'Une grande architecture comporte de grandes responsabilités - Clean Architecture - VIPER (version iOS)',
+          abstract:
+            "Comme vous le savez, chaque développeur est une sorte de super-héros de son application. Il garantit qu'il est évolutif, facilement testable et maintenable. Nous allons passer ensemble par une approche d'architecture qui nous permet d'avoir une application robuste.",
+          tags: ['clean architecture', 'solid', 'architecture', 'ios'],
+          lang: ['fr'],
+        },
+        {
+          title: 'Jenkins Pipeline - Un ami qui vous veut du bien. (version iOS)',
+          abstract:
+            'C\'est un gars bien. Il est toujours là pour aider. Il est peut-être en retrait dans ton équipe ou bien tu l\'as aperçu chez d\'autres, et tu aimerais le connaître davantage. Il a plusieurs casquettes mais il est hyper cool pour de la CI / CD.',
+          tags: ['code', 'qualité', 'ci', 'cd', 'integration continue', 'deploiement continue', 'jenkins', 'pipeline', 'ios'],
+          lang: ['fr'],
+        },
+        {
+          title: 'Les bonnes pratiques pour faire des tests unitaires',
+          abstract:
+            'Nous allons voir ensemble certaines principes de base tel que l\'injection de dépendance, S.O.L.I.D... Afin de réaliser des tests unitaires',
+          tags: ['code', 'qualité', 'solid', 'tests', 'tests unitaires', 'injection de dépendance', 'ios'],
+          lang: ['fr'],
+        },
+      ],
+      cities: ['Paris'],
+      contacts: {
+        twitter: 'IlanBenhamou',
+      },
+    },
   ],
   cities: {
     'Aix-en-Provence': {
