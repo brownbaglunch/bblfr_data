8851a89d3c5295d81653c7a5c169916ff6de5728
Jean-Phi Baconnais <jeanphilippe.baconnais@gmail.com>
2019-12-13 08:01:14 +0100

    add conf gitlab (#424)

--- baggers.js
+++ baggers.js
@@ -16338,6 +16338,13 @@ var data = {
           tags: ['Dev', 'Java', 'Kotlin'],
           lang: ['fr'],
         },
+        {
+          title: 'GitlabCI : Make CI/CD (with Gitlab CI) hype again ?',
+          abstract:
+            "Vous vous prenez la tête pour faire de l'intégration continue ? En plus ça tombe mal vos OPS ne sont pas dispos ? Gitlab met en place depuis quelques années des fonctionnalités permettant de gérer des pipelines de déploiement dans le même outil de gestion de sources. Nous ne travaillons pas pour Gitlab (nous n’avons aucune action promis ^^), et nous ne voulons pas remplacer les OPS mais nous allons vous montrer comment Gitlab Ci peut vous permettre de gérer vos pipelines directement dans votre gestionnaire de conf préféré sans rien installer et en quelques minutes. Nous vous montrerons aussi comment pousser un peu plus Gitlab Ci dans ses retranchements et en tirer le meilleur.",
+          tags: ['Dev', 'CICD', 'Gitlab'],
+          lang: ['fr'],
+        },
       ],
       cities: ['Nantes'],
       contacts: {
