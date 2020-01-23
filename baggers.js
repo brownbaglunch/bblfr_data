5cbb5d60f8ad364815a04fcebcd654526ca6c967
Constantin Guay <const.g@gmail.com>
2020-01-23 11:51:32 +0100

    update Constantin Guay profile

--- baggers.js
+++ baggers.js
@@ -14955,7 +14955,7 @@ var data = {
       cities: ['Paris', 'Versailles'],
       contacts: {
         twitter: 'cog_g',
-        mail: 'const.g@gmail.com',
+        mail: 'constantin.guay@gmail.com',
       },
       websites: [
         {
@@ -15042,7 +15042,7 @@ var data = {
           lang: ['fr', 'en'],
         },
         {
-          title: 'Story Mapping : Atelier',
+          title: 'User Story Mapping : Atelier',
           abstract:
             "Le meilleur moyen de comprendre le besoin du client c'est en étudiant son workflow et en en tirant des hypothèses sur son besoin",
           tags: [
@@ -15098,7 +15098,7 @@ var data = {
           lang: ['fr', 'en'],
         },
         {
-          title: "#NoEstimate : Ca veut dire quoi ? Comment s'y prendre ?",
+          title: "Peut-on se passer des estimations ? Comment s'y prendre ?",
           abstract:
             'Une entrée en matière sur le mouvement NoEstimate, plus mes astuces et conseils pour le mettre en place chez vous, que ce soit avec une nouvelle équipe ou sur une équipe qui fonctionne déjà tout en répondant aux besoins de roadmap et budget.',
           tags: [
