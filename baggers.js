f87e965c7055fa411e391457995a8154613eb640
Eric Lefevre-Ardant <contact@ericlefevre.net>
2020-03-10 10:22:29 +0100

    Retrait de présentation pour Eric LA (#435)

--- baggers.js
+++ baggers.js
@@ -1354,7 +1354,7 @@ var data = {
     {
       since: '2013-03-29',
       name: 'Eric Lefevre-ardant',
-      bio: 'Développeur chez leboncoin',
+      bio: 'Développeur chez Hyperlex',
       picture: 'img/baggers/photo_eric.jpg',
       websites: [
         {
@@ -1364,13 +1364,6 @@ var data = {
       ],
       location: 'Paris',
       sessions: [
-        {
-          title: 'Découvrez le langage Go',
-          abstract:
-            "Vous êtes développeur Java ou C ? Je vous propose de découvrir le langage Go, qui fait parler de lui dans un contexte d'architecture Microservice. Cette introduction comparera Java et Go et vous montrera comme il est facile d'implémenter des services HTTP qui s'exécutent de façon concurrente.",
-          tags: ['go', 'java'],
-          lang: ['fr'],
-        },
         {
           title: "Introduction à la Philosophie de l'intelligence artificielle",
           abstract:
@@ -1382,13 +1375,6 @@ var data = {
           title: 'Boucles Etranges : Etranges Boucles - avec Guillaume Tardif',
           abstract:
             "Une promenade décousue dans les limites du code source Java, à la manière de Douglas Hofstadter. Nous parlons de code, d'apparences trompeuses, d'art et de la vie.",
-          tags: ['Java', 'Code'],
-          lang: ['fr'],
-        },
-        {
-          title: 'Extremist Programming - avec Cyrille Martraire',
-          abstract:
-            "Bien sûr, notre métier est sérieux ; le Software Craftmanship n'est-il pas avant tout le professionnalisme ? Bien sûr, le progrès vient d'articles scientifiques ardus et de nouveaux logiciels complexes. Mais vous ne rêvez pas parfois de repousser le cadre professionnel habituel ? D'aller plus loin et d'explorer ? De vous amuser avec du code jusqu'à ce que ça devienne n'importe quoi ? Dans cette session nous vous présenterons des expériences ésotériques, des jeux bizarres et des joyeux délires qui font réfléchir. Réfléchir ? D'habitude on n'a jamais le temps !",
           tags: ['Java', 'Code'],
           lang: ['fr'],
         },
