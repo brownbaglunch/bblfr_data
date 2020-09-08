07ed1d645b5d4540c345cee1b21d4d3d64adb128
Delphine Raymond <draymond@lucca.fr>
2020-09-08 18:03:13 +0200

    Update baggers.js
    Sorry @dadoonet I added one mistake, could you merge it?

--- baggers.js
+++ baggers.js
@@ -17735,7 +17735,6 @@ var data = {
           abstract:
             "Vous souhaitez découvrir un atelier d'Event Storming ou en animer un pour partager sur le métier de votre produit ou d'une nouvelle fonctionnalité en équipe, afin d'avoir tous la même compréhension du métier et parler le même lanagage.",
           tags: ['Event Storming', 'Workshop', 'Facilitation', 'Agile', 'DDD'],
-          ],
           lang: ['fr'],
         },        
         {
