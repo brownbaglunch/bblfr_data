7edccfa004460b8bc5f88f115d790f8fbd8fec3e
Julien Tanguy <tanguy.j@sfeir.com>
2021-02-10 15:05:31 +0100

    Update baggers.js for Julien Tanguy

--- baggers.js
+++ baggers.js
@@ -7607,11 +7607,11 @@ var data = {
           lang: ['fr'],
         },
         {
-            title: 'Atelier Event Storming',
-            abstract:
+          title: 'Atelier Event Storming',
+          abstract:
             "Un atelier pour découvrir cette technique collaborative qui vise à modéliser votre métier tout en réunissant ceux qui savent ce qu'il faut réaliser et ceux qui savent comment le faire. Mise en application en live sur votre propre métier.",
-            tags: ['EventStorming', 'Ubiquitous Language', 'Workshop', 'DDD'],
-            lang: ['fr'],
+          tags: ['EventStorming', 'Ubiquitous Language', 'Workshop', 'DDD'],
+          lang: ['fr'],
         },
         {
           title: 'Atelier Architecture: C4 Model',
@@ -11874,13 +11874,13 @@ var data = {
     {
       since: '2017-04-21',
       name: 'Julien Tanguy',
-      bio: 'Développeur backend/frontend - Valwin',
+      bio: 'Développeur backend/frontend - SFEIR Nantes',
       picture:
         'https://pbs.twimg.com/profile_images/711247904661499907/NyupMTjB_400x400.jpg',
       websites: [
         {
           name: 'Web',
-          url: 'https://jtanguy.cleverapps.io',
+          url: 'https://jtanguy.me',
         },
       ],
       cities: ['Nantes'],
@@ -11889,6 +11889,27 @@ var data = {
         mail: 'julien.tanguy@jhome.fr',
       },
       sessions: [
+        {
+          title: "Introduction à l'EventSourcing",
+          abstract:
+            "L'event sourcing nous offre une façon différente de stocker la donnée: au lieu de stocker l'état de notre application à un instant t, on stocke les différents évènements, les faits passés. À partir de ces événements, il nous est possible de reconstruire n'importe quel état à n'importe quel instant, ce qui nous offre une grande flexibilité pour réorganiser notre application, voire même de corriger des bugs a posteriori.",
+          tags: ['EventSourcing', 'Architecture'],
+          lang: ['fr'],
+        },
+        {
+          title: 'Introduction à Webpack',
+          abstract:
+            'Webpack est un packager qui vous permet de gérer tout votre arbre de dépendances JS/(S)CSS/Images/Json, idéal pour les frameworks javascript frontend.',
+          tags: ['JavaScript', 'Webpack'],
+          lang: ['fr'],
+        },
+        {
+          title: 'Débugger sa configuration Webpack',
+          abstract:
+            "Le nouveau plugin webpack que vous venez d'installer vient de tout casser ? Vous n'arrivez pas à mettre à mettre à jour webpack ? Nous verrons ensemble comment comprendre une configuration webpack et se donner des outils pour l'améliorer.",
+          tags: ['JavaScript', 'Webpack'],
+          lang: ['fr'],
+        },
         {
           title: 'Débuter en React/Redux',
           abstract:
@@ -11901,13 +11922,6 @@ var data = {
           abstract:
             'Elm (elm-lang.org) est un language de programmation pour créer des applications web facile à prendre en main. Compilé en JS, il peut être embarqué dans de nombreuses applications et sites web.',
           tags: ['Elm', 'Programmation fonctionnelle'],
-          lang: ['fr'],
-        },
-        {
-          title: 'Introduction à Webpack',
-          abstract:
-            'Webpack est un packager qui vous permet de gérer tout votre arbre de dépendances JS/(S)CSS/Images/Json, idéal pour les frameworks javascript frontend.',
-          tags: ['JavaScript', 'Webpack'],
           lang: ['fr'],
         },
       ],
