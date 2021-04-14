11c5b0b222b0d059ed86cfbe7eb0f1de7d5be410
Graveaud Geoffrey <zenika-ggr@graveauds-mbp.home>
2021-04-14 15:50:26 +0200

    chore: add Geoffrey Graveaud as a speaker and 1 topic about Accelerate

--- baggers.js
+++ baggers.js
@@ -17718,6 +17718,33 @@ var data = {
         },
       ],
     },
+    {
+      since: '2021-04-14',
+      name: 'Geoffrey Graveaud',
+      bio: 'Consultant Devops, passionné du domaine informatique',
+      picture:
+        'https://media-exp1.licdn.com/dms/image/C4D03AQE66hlp0YItFg/profile-displayphoto-shrink_800_800/0/1572017655686?e=1623888000&v=beta&t=oazW2XaSCVotoq1aHIKQsqR_9y3EX9T-yn1ArKJAkd0',
+      cities: ['Bordeaux'],
+      contacts: {
+        twitter: 'GraveaudG',
+        mail: 'geoffrey.graveaud@zenika.com',
+      },
+      sessions: [
+        {
+          title: 'A la découverte d Accelerate (La science derrière DevOps),
+          abstract:
+            "Comment être plus productif et qualitatif ou bien rentable et stable ? Faire des choix stratégiques pour son SI et son business n'est jamais évident dans un marché fortement concurrentiel. Il faut pouvoir définir la meilleure façon d'utiliser l'informatique au service de l'entreprise. Accelerate (la science derrière DevOps) est une recherche de 4 ans qui a défini les pratiques informatiques pour améliorer sa performance organisationnelle: productivité, rentabilité et être plus concurrentiel sur le marché. La force d'Accelerate est que c'est applicable à toutes les entreprises (note inscrite dans la recherche) et aide aux choix stratégiques dans son entreprise. A travers cet atelier, vous comprendrez l'idée générale d'accelerate, quelques mécanismes et les résultats qu'Accelerate prédit.",
+          tags: ['Workshop', 'DevOps', 'Lean', 'Agilité'],
+          lang: ['fr'],
+        },
+      ],
+      websites: [
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/geoffrey-graveaud-033319b0/',
+        },
+      ],
+    },
   ],
   cities: {
     'Aix-en-Provence': {
