e39ea1fc00b3275043f3d1083ea4582a1cfa3773
David Pilato <david@pilato.fr>
2021-05-03 15:50:29 +0200

    Fix error

--- baggers.js
+++ baggers.js
@@ -17731,7 +17731,7 @@ var data = {
       },
       sessions: [
         {
-          title: 'A la découverte d Accelerate (La science derrière DevOps),
+          title: 'A la découverte d Accelerate (La science derrière DevOps)',
           abstract:
             "Comment être plus productif et qualitatif ou bien rentable et stable ? Faire des choix stratégiques pour son SI et son business n'est jamais évident dans un marché fortement concurrentiel. Il faut pouvoir définir la meilleure façon d'utiliser l'informatique au service de l'entreprise. Accelerate (la science derrière DevOps) est une recherche de 4 ans qui a défini les pratiques informatiques pour améliorer sa performance organisationnelle: productivité, rentabilité et être plus concurrentiel sur le marché. La force d'Accelerate est que c'est applicable à toutes les entreprises (note inscrite dans la recherche) et aide aux choix stratégiques dans son entreprise. A travers cet atelier, vous comprendrez l'idée générale d'accelerate, quelques mécanismes et les résultats qu'Accelerate prédit.",
           tags: ['Workshop', 'DevOps', 'Lean', 'Agilité'],
