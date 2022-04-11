83ca23d2a600619158d158a09dfcc9d473ea7d3c
Your Name <sfauvel@gmail.com>
2022-04-11 09:06:48 +0200

    Fix quote

--- baggers.js
+++ baggers.js
@@ -13316,7 +13316,7 @@ var data = {
         {
           title: 'Living documentation as test',
           abstract:
-            'Maintenir la documentation sur un projet est un vrai challenge. J\explore depuis quelques temps la production d\'une Living Documentation validant l\'application et se substituant aux tests. Je propose de vous décrire le concept, soit à travers une présentation, soit en animant un coding dojo, soit en travaillant directement sur votre application.',
+            'Maintenir la documentation sur un projet est un vrai challenge. J\'explore depuis quelques temps la production d\'une Living Documentation validant l\'application et se substituant aux tests. Je propose de vous décrire le concept, soit à travers une présentation, soit en animant un coding dojo, soit en travaillant directement sur votre application.',
           tags: ['Documentation', 'Test', 'Dojo'],
           lang: ['fr'],
         },
