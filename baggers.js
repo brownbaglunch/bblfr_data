5cdd07f204ffba9e6d26247c0c5d54adcd1b8070
Jean-François Garreau - Binomed <jean.francois.garreau@gmail.com>
2022-04-11 10:32:24 +0200

    Update baggers.js
    Co-authored-by: Romain Linsolas <linsolas@gmail.com>

--- baggers.js
+++ baggers.js
@@ -5499,7 +5499,7 @@ var data = {
           lang: ['fr'],
         },
         {
-          title: 'Explorons les stratégies de caches offertent sur les PWA',
+          title: 'Explorons les stratégies de cache offertes sur les PWA',
           abstract:
             'Quand on parle de PWA, il est question de Service Workers, c\'est un fait. Etant donné que ce dernier nous permet de servir des fichiers hors-ligne, il est donc pertinent de se pencher sur les mécanismes de mises en cache proposés par ce dernier.\nA travers cette sessions, nous vous proposons de balayer les différentes et plus connues stratégies de mises en cache d\'une PWA. Nous tacherons pour chacune de ces stratégies de vous proposer un exemple d\'implémentation.\nNous mettrons aussi le nez dans workbox qui pourra vous aider dans la mise en place de ces dernières.\nLes stratégies explorées seront les suivantes :\n* Cache-Only\n* Network-Only\n* Cache-First\n* Network-First\n* Cache-Then-Network\n* Stale-While-Revalidate\n* Generic-Fallback',
           tags: ['PWA', 'ServiceWorker'],
