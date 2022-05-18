ba394bf975bf44e0ad60011fde2f1945dc5b9987
Pierre Baillet <pierre@baillet.name>
2022-05-18 22:52:07 +0200

    Update baggers.js
    Co-authored-by: Romain Linsolas <linsolas@gmail.com>

--- baggers.js
+++ baggers.js
@@ -6883,7 +6883,7 @@ var data = {
         { 
           title: 'Le Chaos Engineering pour les nuls',
           abstract: 'Vos services sont les plus robustes du monde ? Êtes vous prêt à passer à la vitesse supérieure ? Découvrez le Chaos Engineering, fils fou et prodige des SRE et de l\'armée de 12 singes.',
-          tags: ['Chaos', 'Monitoring', 'Robustness', 'Reliability']
+          tags: ['Chaos', 'Monitoring', 'Robustness', 'Reliability'],
           lang: ['fr']
         },
         {
