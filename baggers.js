42cae365e9a158e6d3633e5a6a45fab1123c419a
StephLangFr <52566576+StephLangFr@users.noreply.github.com>
2022-12-19 15:55:19 +0100

    Update baggers.js

--- baggers.js
+++ baggers.js
@@ -17984,6 +17984,65 @@ var data = {
         mail: 'laurent.doguin@couchbase.com',
       },
     },
+  {
+  "since": "2022-12-06",
+  "name": "Stéphane Lang",
+  "bio": "Passionné par les bases de données distribuées relationnelles et noSQL | Ingénieur solutions chez Couchbase",
+  "picture": "https://secure.gravatar.com/avatar/f2c7c409a480e8a5e7891220826315f1?size=200",
+  "location": "Paris, Lille, remote",
+  "sessions": [
+    {
+      "title": "Introduction à Couchbase et aux bases de données NoSQL",
+      "abstract": "Le NoSQL, pour quelles applications, quels cas d'usage? Couchbase est une plateforme de données multi-services, deployable partout jusque sur mobiles. Elle combine à la fois performances, requêtes SQL, recherche full-text et plus encore pour rationnalier l'écosystème de solutions des entreprises. Tour d'horizon de l'architecture et des possibilités de Couchbase, la base NoSQL qui aime le SQL",
+      "tags": [
+        "NoSQL",
+        "Couchbase",
+        "Base de données",
+        "JSON"
+      ],
+      "lang": [
+        "fr"
+      ]
+    },
+    {
+  "title": "Applications mobiles déconnectées",
+  "abstract": "Les applications mobiles doivent fournir une expérience utilisateur flexible, riche et sans accroc quelle que soit la couverture réseau. Couchbase propose une base de données NoSQL embarquée disponible offline qui se synchronise automatiquement. C'est la solution de référence pour les plus grands acteurs de toutes les industries",
+  "tags": [
+    "mobile",
+    "offline",
+    "Base de données",
+    "JSON",
+    "SQLite"
+  ],
+  "lang": [
+    "fr"
+  ]
+},
+    {
+  "title": "Bases de données distribuées - Faire le bon choix d'architecture et de cohérence",
+  "abstract": "Les bases de données distribuées deviennent une nécessité. Mais chaque techno vient avec ses choix d'architectures et de gestion de la cohérence. Ces choix sont structurant et ils impactent les applications. C'est pourquoi ils doivent être évalués attentivement. Dans cette session d'introduction, nous abordons les concepts qui entrent en jeu et les points d'attention liés aux architecture des bases de données relationnelles et NoSQL les plus populaires",
+  "tags": [
+    "architecture",
+    "base de données",
+    "consistency",
+    "noSQL"
+  ],
+  "lang": [
+    "fr"
+  ]
+}
+  ],
+  "cities": [
+    "Paris",
+    "Lille",
+    "Nantes",
+    "Lyon",
+    "Bordeaux"
+  ],
+  "contacts": {
+    "mail": "steph.lang.fr@gmail.com"
+  }
+}
   ],
   cities: {
     'Aix-en-Provence': {
