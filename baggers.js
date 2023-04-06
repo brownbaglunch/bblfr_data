e4f2d19520b665d6b2654e1c1cc33b1a2f9f121e
Alizée Gottardo <alizee.gottardo@osedea.com>
2023-04-06 16:28:43 +0200

    Added Alizée Gottardo as bagger

--- baggers.js
+++ baggers.js
@@ -17981,6 +17981,46 @@ var data = {
         mail: 'laurent.doguin@couchbase.com',
       },
     },
+    {
+      since: '2023-04-06',
+      name: 'Alizée Gottardo',
+      bio:
+        "Software Developer & Business Analyst @OSEDEA. L'approche que je privilégie : explorer et modéliser le domaine métier pour concevoir des produits qui ont du sens. J'adore échanger autour du Domain Driven Design et de l'éco-conception.",
+      picture:
+        'https://en.gravatar.com/userimage/73573562/d450ac63ef86c12c106c63042c02c3ce.png?size=200',
+      websites: [
+        {
+          name: 'Linkedin',
+          url: 'https://www.linkedin.com/in/alizeegottardo/',
+        },
+        {
+          name: 'Website',
+          url: 'https://www.osedea.com/fr',
+        },
+      ],
+      location: 'Nantes',
+      sessions: [
+        {
+          title:
+            "De la conception à l'implémentation : un atelier pour faciliter la collaboration entre designers et développeur·euse·s",
+          abstract:
+            "Je vous propose un atelier co-animé avec un.e designer pour identifier les problématiques de collaboration que vous rencontrez au sein de votre équipe de designers et développeur·euse·s. Nous vous aiderons à trouver des pistes de solutions en vous partageant notre retour d'expérience.",
+          tags: ['Design', 'Développement', 'Collaboration'],
+          lang: ['fr'],
+        },
+        {
+          title: 'Pourquoi mettre en place une équipe Platform Engineering ?',
+          abstract:
+            "Avec le passage à l'ère Full Cycle, les développeur·euse·s ont maintenant à leur charge l'entièreté du cycle de développement : concevoir, développer, tester, déployer, opérer et maintenir. Le rôle d'une équipe Platform Engineering est de venir supporter les équipes produit pour leur permettre de rester concentrées sur le domaine métier.",
+          tags: ['Développement', 'Platform Engineering', 'Full Cycle'],
+          lang: ['fr', 'en'],
+        },
+      ],
+      cities: ['Nantes'],
+      contacts: {
+        mail: 'alizee.gottardo@osedea.com',
+      },
+    },
   ],
   cities: {
     'Aix-en-Provence': {
