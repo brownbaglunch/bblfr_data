de48e7042d8fd534e692262feccf7db9fd6407a5
julienTroopers <91535619+julienTroopers@users.noreply.github.com>
2023-04-28 12:27:49 +0200

    add(bager): Cécile Staehle (Nantes) (#494)
    Co-authored-by: Romain Linsolas <linsolas@gmail.com>

--- baggers.js
+++ baggers.js
@@ -18203,6 +18203,45 @@ var data = {
       },
     },
     {
+      since: '2023-04-27',
+      name: 'Cécile Staehle',
+      bio:
+        'Je suis associée coopératrice de la SCOP Troopers, en charge du Développement RH (culture d\'entreprise et gouvernance, santé et sécurité, recrutement et onboarding, marque employeur, formation, etc.). Je porte et développe des projets au service de l\'épanouissement des Troopers, dans des conditions de travail respectueuses, bienveillantes et inclusives. Notre entreprise est une agence spécialisée dans la création et refonte de sites web et applications métier complexes. Nous pratiquons l\'autogestion et fonctionnons en gouvernance partagée et participative, nous militons pour une tech au service du bien commun, équitable et respectueuse de chacune et chacun, nous défendons le numérique responsable aussi bien dans nos métiers que dans nos engagements sociaux.',
+      picture:
+        'https://strapi.troopers.agency/uploads/cecile_9ee7f62fb0.jpeg',
+      websites: [
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/cécile-staehle/',
+        },
+        {
+          name: 'Web',
+          url: 'https://troopers.coop',
+        },
+      ],
+      location: 'Nantes',
+      sessions: [
+        {
+          title: 'Management participatif : mieux décider ensemble grâce aux outils de la sociocratie',
+          abstract:
+            "Lorsqu'il s'agit de prendre des décisions ou faire des choix stratégiques, on confie cette responsabilité aux managers, aux N+1, voire aux dirigeant·es. Mais avez-vous déjà pensé à impliquer l'ensemble de l'équipe ? Mais à plusieurs, ce n’est pas  simple d’atteindre le consensus. Savez-vous que ce n’est pas le seul mode de décision possible ? Ensemble, nous découvrirons la décision par consentement à l’aide d’une mise en situation concrète.",
+          tags: ['sociocratie', 'organisation', 'décision', 'gouvernance', 'consentement'],
+          lang: ['fr'],
+        },
+        {
+          title: 'Pyramide, cercles, vertical : ceci n’est pas un cours de géométrie',
+          abstract:
+            "Dans cet atelier de découverte, nous ferons un état des lieux rapide du management en entreprise et, grâce à une expérimentation ludique, nous aborderons le sujet de la gouvernance partagée et participative, comment cela fonctionne, quelles transformations cela implique.",
+          tags: ['management', 'organisation', 'gouvernance', 'autogestion', 'sociocratie'],
+          lang: ['fr'],
+        },
+      ],
+      cities: ['Nantes'],
+      contacts: {
+        mail: 'cecile@troopers.email',
+      },
+    },
+    {
       since: '2023-04-28',
       name: 'Julien Wittouck',
       bio:
