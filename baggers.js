d3318cb8f38ad0ea997427da963a249c57c36b96
arnaud <arnaud.langlade@gmail.com>
2023-06-08 09:45:16 +0200

    Ajout session Arnaud Langlade

--- baggers.js
+++ baggers.js
@@ -11153,7 +11153,7 @@ var data = {
       since: '2023-01-04',
       name: 'Arnaud Langlade',
       bio:
-        "Comprendre le problème que j'essaie de résoudre compte réellement pour moi. C'est pourquoi je suis un grand fan d'ateliers comme l’event storming ou l’example mapping. J'aime tout ce qui se termine par *DD (DDD, BDD et TDD), ces outils et toutes les valeurs d'eXtreme Programming m'aident à développer des applications.",
+        "Je suis ingénieur logiciel en freelance. Je suis un grand fan d'ateliers comme l’event storming ou l’example mapping car comprendre le problème que j'essaie de résoudre est essentiel pour moi. J'aime tout ce qui se termine par *DD (DDD, BDD et TDD), ces outils et toutes les valeurs d'eXtreme Programming m'aident à développer des applications depuis pas mal d’années.",
       picture:
         'https://s.gravatar.com/avatar/3566a0bf63ad474de1e2d8c35c5d76a1?s=300',
       websites: [
@@ -11182,6 +11182,14 @@ var data = {
           abstract:
             "Pendant pas mal d'années, j’ai préparé les sujets à venir avec de longs et ennuyeux groomings. Il était très compliqué de rester concentré jusqu’à la fin. Une réunion trop longue ne favorise pas le partage et l’alignement des équipes sur ce qui est attendu. N’oublions pas que c’est l’incompréhension des développeurs qui part en production comme le disait Alberto Brandolini. Durant cette présentation, je vais vous parler d’un atelier appelé “Example Mapping”. Cet atelier vous permettra facilement de communiquer les besoins et de construire votre backlog en équipe.",
           tags: ['bdd', 'example-mapping', 'atelier'],
+          lang: ['fr'],
+        },
+        {
+          title:
+            'Tests unitaires: essentiels et compliqués à la fois',
+          abstract:
+            "Je me souviens lorsque j’ai commencé à développer que je pouvais coder pendant des heures sans exécuter mon code. La suite était moins marrante, je passais des heures à le débugger. J’ai découvert les tests par la suite et j’ai rapidement compris les avantages qu’ils apportaient. Cependant, ça n’a pas été facile d’écrire mes premiers tests. On peut commettre beaucoup d'erreurs qui les rendent difficiles à écrire et à maintenir. Je souhaite vous présenter ce que j’ai appris durant ces dernières années afin de faciliter l’écriture de vos tests.",
+          tags: ['unit-test', 'POO'],
           lang: ['fr'],
         },
       ],
