869f87c5fe52a06cbb369501c9a724c353241b2c
Fabien Brunet <dev@linktogo.fr>
2023-06-27 14:18:08 +0200

    add fabien brunet (#501)
    * add fabien brunet
    
    * Update baggers.js
    
    Co-authored-by: Romain Linsolas <623217+linsolas@users.noreply.github.com>
    
    * Update baggers.js
    
    Co-authored-by: Romain Linsolas <623217+linsolas@users.noreply.github.com>
    
    * Update baggers.js
    
    Co-authored-by: Romain Linsolas <623217+linsolas@users.noreply.github.com>
    
    * Update baggers.js
    
    Co-authored-by: Romain Linsolas <623217+linsolas@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Romain Linsolas <623217+linsolas@users.noreply.github.com>

--- baggers.js
+++ baggers.js
@@ -18402,6 +18402,45 @@ var data = {
         mail: 'marie.desnos@osedea.com',
       },
     },
+    {
+      since: '2023-06-26',
+      name: 'Fabien Brunet',
+      bio: 'Front-end Architect at Allianz Technology. Full stack developer, passionate about new technologies, I have decided to move towards Java and Node applications development. I turn to service delivery as technical referent, focusing on java development technical expertise, human relationships and contacts.',
+      picture: 'https://avatars.githubusercontent.com/u/29751338?v=4',
+      websites: [
+        {
+          name: 'Linkedin',
+          url: 'https://www.linkedin.com/in/fbrunet/',
+        },
+        {
+          name: 'Website',
+          url: 'https://fabien-brunet.fr/',
+        },
+      ],
+      location: 'Rennes, Nantes et Paris',
+      sessions: [
+        {
+          title:
+            "Comment nous avons implementé les Micro-Frontends chez Allianz",
+          abstract:
+            "Je vous propose un retour d'expérience autour des Micro-Frontends que nous avons implémentés chez Allianz Technology. Durant ce talk, je vous montrerai et décrirai notre cheminement de simple SPA aux Micro-Frontends : l'existant, les multi et mono repos, sécurité et Micro-Frontend, nos points de blocage et difficultés, nos réussites et conclusions",
+          tags: ['Frontend', 'Développement', 'Architecture'],
+          lang: ['fr'],
+        },
+        {
+          title:
+            "Eco-responsabilité et développement chez Allianz",
+          abstract:
+            "Depuis 3 ans, un groupe de travail collabore autour du sujet de l'empreinte carbone chez Allianz et les usages pour la réduire. Fort et fière de sa communauté de développeurs, le GreenIT progresse dans l'écosystème d'Allianz. Durant ce talk, je vous montrerai et décrirai notre cheminement de ces 3 ans : avancées, points de blocage, difficultés et réussites.",
+          tags: ['Frontend', 'Développement', 'Sustainable'],
+          lang: ['fr'],
+        },
+      ],
+      cities: ['Rennes'],
+      contacts: {
+        mail: 'dev@linktogo.fr',
+      },
+    },
   ],
   cities: {
     'Aix-en-Provence': {
