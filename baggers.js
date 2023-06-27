8f03f708a9ffdef94b082633a287cd638ee06a46
Fabien Brunet <dev@linktogo.fr>
2023-06-27 11:38:06 +0200

    Update baggers.js
    Co-authored-by: Romain Linsolas <623217+linsolas@users.noreply.github.com>

--- baggers.js
+++ baggers.js
@@ -18421,7 +18421,7 @@ var data = {
       sessions: [
         {
           title:
-            "Comment nous avons implementez les Micros Frontend chez Allianz",
+            "Comment nous avons implementé les Micro-Frontends chez Allianz",
           abstract:
             "Je vous propose un retour d'expérience autour des Micro-Frontends que nous avons implémentés chez Allianz Technology. Durant ce talk, je vous montrerai et décrirai notre cheminement de simple SPA aux Micro-Frontends : l'existant, les multi et mono repos, sécurité et Micro-Frontend, nos points de blocage et difficultés, nos réussites et conclusions",
           tags: ['Frontend', 'Développement', 'Architecture'],
