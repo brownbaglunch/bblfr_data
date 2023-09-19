13535d87823ac1446f2c66c245a07e3fdb84415a
paul <paul@troopers.email>
2023-09-19 14:24:59 +0200

    Add Paul Andrieux

--- baggers.js
+++ baggers.js
@@ -18441,6 +18441,44 @@ var data = {
         mail: 'dev@linktogo.fr',
       },
     },
+    {
+      since: "2023-09-19",
+      name: "Paul Andrieux",
+      bio: "Fondateur de l'agence Troopers, je suis développeur de formation. Intéressé par les sujets DevOps et les formes alternatives de management, notamment l'auto-gestion. ",
+      picture: "https://0.gravatar.com/avatar/f408278b6ffa94351c033f9e006847f2b57ff04cdc15e7239db3fb9592369d55?size=200",
+      websites: [
+        {
+          name: "Web",
+          url: "https://troopers.coop/"
+        },
+        {
+          name: "LinkedIn",
+          url: "https://www.linkedin.com/in/paulandrieux/"
+        }
+      ],
+      location: "Nantes, quartier de la Madeleine",
+      sessions: [
+        {
+          title: "Mettre en oeuvre une pratique d’intégration continue, GitLab à la rescousse",
+          abstract: "Vous voulez améliorer la qualité de votre code tout en accélérant la livraison de nouvelles fonctionnalités ? GitLab peut pour vous aider. Dans ce BBL, on va explorer ensemble les étapes à suivre pour intégrer GitLab dans votre processus de développement. On va parler d'automatisation de tests, de déploiements, et de collaboration en équipe. En bref, on va vous donner les premières pistes de mise en place une pratique d'intégration continue efficace !",
+          tags: [
+            "CI",
+            "DevOps",
+            "Qualité logicielle",
+          ],
+          lang: [
+            "fr"
+          ]
+        },
+      ],
+      cities: [
+        "Nantes"
+      ],
+      contacts: {
+        twitter: "paulandrieux",
+        mail: "paul@troopers.email"
+      }
+    },
   ],
   cities: {
     'Aix-en-Provence': {
