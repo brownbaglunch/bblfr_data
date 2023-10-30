da6a04f311e5260b707163ebaa3653f324152794
lou-theo <28760135+lou-theo@users.noreply.github.com>
2023-10-30 16:52:58 +0100

    update baggers : Lou-Théo LAURENT (#506)

--- baggers.js
+++ baggers.js
@@ -18525,6 +18525,51 @@ var data = {
         mail: "paul@troopers.email"
       }
     },
+    {
+      since: '2023-10-28',
+      name: 'Lou-Théo LAURENT',
+      bio: 'Développeur curieux, touche à pas mal de choses. J\'adore les événements que ce soit en tant que participant ou organisateur. La qualité me tient à coeur, je progresse pour l\'améliorer.',
+      picture: 'https://media.licdn.com/dms/image/C4D03AQGhAgP_RVN3Fg/profile-displayphoto-shrink_400_400/0/1561902693360?e=1703721600&v=beta&t=cUyKHv0xiEM6TVVERY3FGgQ-WOCctfobQ0ZUZ_X1nqU',
+      location: 'Strasbourg et environs',
+      cities: ['Strasbourg'],
+      contacts: { mail: 'laurent.lt@sfeir.com' },
+      websites: [
+        {
+          name: 'Github',
+          url: 'https://github.com/lou-theo/',
+        },
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/lou-theo-laurent/',
+        },
+        {
+          name: 'Web',
+          url: 'https://www.sfeir.dev/author/loutheo-laurent/',
+        },
+      ],
+      sessions: [
+        {
+          "title": "La réactivité dans Angular, comment ça fonctionne ?",
+          "abstract": "Vous qui utilisez Angular au quotidien, avez-vous déjà eu la curiosité de savoir comment le framework fonctionne ? Comment votre composant sait-il qu'il doit se rafraichir ? Quelles sont les possibilités pour optimiser notre application ? Que nous réserve le futur ?",
+          "tags": [
+            "JavaScript",
+            "TypeScript",
+            "Angular",
+          ],
+          "lang": [ "fr" ]
+        },
+        {
+          "title": "Quel futur pour Angular ? (vu de 2023-2024)",
+          "abstract": "Angular a manifesté des changements impressionnants depuis quelques versions et cela n’est rien face à ce qu’il y a dans les cartons ! En plus des Signals, le phénomène qui va révolutionner la détection de changement dans Angular, nous discuterons également du futur système de contrôle des flux (le remplaçant des ngIf & co) envisagé pour la partie template. Accrochez-vous, vous risquez d’être choqué !",
+          "tags": [
+            "JavaScript",
+            "TypeScript",
+            "Angular",
+          ],
+          "lang": [ "fr" ]
+        },
+      ]
+    }
   ],
   cities: {
     'Aix-en-Provence': {
