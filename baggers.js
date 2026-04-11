1ac6d83f32fa14fb04a76cf70e52fe49cca3df03
Jean-Phi Baconnais <jean-philippe.baconnais@zenika.com>
2026-04-11 12:28:04 +0200

    :sparkles: Update my information (#527)

--- baggers.js
+++ baggers.js
@@ -15993,16 +15993,25 @@ var data = {
     {
       since: '2019-04-16',
       name: 'Jean-Philippe Baconnais',
-      bio: 'Consultant chez Zenika Nantes',
+      bio:
+        'Dev / Teach Lead chez Zenika Nantes | Co orga Human Talks Nantes & GitLab France | GitLab Heroes, Gitpod Hero & Google Cloud Innovator',
       picture: 'https://s.gravatar.com/avatar/5d2655822def2e889a17ff94e95309e3',
       websites: [
         {
           name: 'Github',
-          url: 'https://github.com/jeanphibaconnais',
+          url: 'https://github.com/jeanphi-baconnais',
         },
         {
           name: 'GitLab',
           url: 'https://gitlab.com/jeanphi-baconnais',
+        },
+        {
+          name: 'Blog',
+          url: 'https://jeanphi-baconnais.gitlab.io/',
+        },
+        {
+          name: 'Bluesky',
+          url: 'https://bsky.app/profile/jeanphi-baconnais.gitlab.io',
         },
       ],
       location: 'Nantes',
@@ -16016,14 +16025,7 @@ var data = {
           lang: ['fr'],
         },
         {
-          title: '[Quicky] - Entamer une migration Kotlin dans une DSI Java',
-          abstract:
-            "Au sein de la DSI de Pôle emploi, les infrastructures black end sont depuis une dizaine d’années faites en Java et le choix des langages n’est pas (encore) déterminé par les équipes de développement. Kotlin utilisant la JVM, il est désormais possible d’intégrer du Kotlin dans des projets java. Dans ce REX je vous présenterai la manière dont nous avons pu entamer l'intégration du Kotlin dans nos composants black end Java, les problématiques rencontrées, mais aussi les opportunités qui s'ouvrent à nous pour la suite.",
-          tags: ['Dev', 'Java', 'Kotlin'],
-          lang: ['fr'],
-        },
-        {
-          title: 'GitlabCI : Make CI/CD (with Gitlab CI) hype again ?',
+          title: 'GitLabCI : Make CI/CD (with Gitlab CI) hype again ?',
           abstract:
             "Vous vous prenez la tête pour faire de l'intégration continue ? En plus ça tombe mal vos OPS ne sont pas dispos ? Gitlab met en place depuis quelques années des fonctionnalités permettant de gérer des pipelines de déploiement dans le même outil de gestion de sources. Nous ne travaillons pas pour Gitlab (nous n’avons aucune action promis ^^), et nous ne voulons pas remplacer les OPS mais nous allons vous montrer comment Gitlab Ci peut vous permettre de gérer vos pipelines directement dans votre gestionnaire de conf préféré sans rien installer et en quelques minutes. Nous vous montrerons aussi comment pousser un peu plus Gitlab Ci dans ses retranchements et en tirer le meilleur.",
           tags: ['Dev', 'CICD', 'Gitlab'],
@@ -16047,14 +16049,36 @@ var data = {
           title:
             'Qui a dit qu’il fallait avoir un JDK sur son poste pour développer une application Java ? Prenez votre IDE Cloud GitPod et le tour est joué 💪',
           abstract:
-            'Quand on arrive sur des projets, la configuration de votre poste est une étape obligatoire : avoir la bonne version du JDK, de Maven ou de librairies. Le tout avec quelques Mo / Go qui s’accumulent sur votre disque dur. Avec l’arrivée des IDE Cloud tels que GitPod, la configuration nécessaire sur un projet est mutualisée sur le cloud et cette étape qui pouvait devenir une réelle galère n’est plus qu’un lointain souvenir ! Après vous avoir présenté en quelques mots GitPod, je vous ferai une démonstration de ce qui est possible pour un dev Java.',
+            'Quand on arrive sur des projets, la configuration de votre poste est une étape obligatoire : avoir la bonne version du JDK, de Maven ou de librairies. Le tout avec quelques Mo / Go qui s’accumulent sur votre disque dur. Avec l’arrivée des IDE Cloud tels que GitPod, la configuration nécessaire sur un projet est mutualisée sur le cloud et cette étape qui pouvait devenir une réelle galère n’est plus qu’un lointain souvenir ! Après vous avoir présenté en quelques mots Gitpod, je vous ferai une démonstration de ce qui est possible pour un dev Java.',
           tags: ['Dev', 'GitPod'],
+          lang: ['fr'],
+        },
+        {
+          title: 'Et si l’IA devenait votre nouvel ami codeur imaginaire ? ',
+          abstract:
+            'Que ce soit dans la rédaction de vos mails, la traduction ou bien la génération d’images, l’intelligence artificielle, ou IA, est partout, je pense que je ne vous apprends rien 😅. En tant que développeur·se, elle a également un rôle à jouer. Avec le projet GitLab Duo, GitLab a intégré dans sa plateforme les premières briques d’IA au sein des différentes étapes DevSecOps de vos projets. De la mise en place d’un chat jusqu’à la revue de code, je vous montrerai comment GitLab peut vous aider à améliorer votre productivité au quotidien grâce à ce nouvel ami imaginaire.',
+          tags: ['Dev', 'GitLab', 'IA'],
+          lang: ['fr'],
+        },
+        {
+          title:
+            'Renovate/Dependabot, ou comment reprendre le contrôle sur la mise à jour de ses dépendances',
+          abstract:
+            "Talk présenté avec Lise Quesnel. Lorsqu’on développe un projet, on se laisse parfois facilement déborder par les mises à jour de dépendances : c’est rébarbatif, répétitif, bref, pas très fun, et personne n’a envie de le faire. Et si une solution automatique le faisait pour nous ou nous aidait à le faire ? C’est la promesse d’outils comme Dependabot ou Renovate qui vont nous proposer des Pull/Merge-requests avec les montées de versions des dépendances de nos projets. Magique non ? Dans ce talk, après vous avoir (re)présenter ces deux outils, nous vous donnerons des conseils sur la mise en place de Renovate. Notre retour est basé sur nos expériences open source ou lors de notre mission (spoiler, bien sur que tout n’a pas fonctionné du premier coup 😅). Ce sera l’occasion de vous faire une démo pour vous permettre de vous rendre compte de l'intérêt (mais pas que) de ces outils et pourquoi pas de vous décider à en installer un sur vos projets !",
+          tags: ['Dev', 'Renovate'],
+          lang: ['fr'],
+        },
+        {
+          title:
+            '🗄️ Et si vous mettiez à disposition vos jobs de CI/CD sur une étagère ?',
+          abstract:
+            'Talk présenté avec Jules Agostini. La CI/CD fait partie de notre quotidien. Que l’on soit développeur·euse, ou bien Ops, nous avons forcément passé du temps à créer ou modifier des scripts de CI/CD. Un nouveau script de CI/CD ? Pas de soucis, nous savons où trouver un exemple et utilisons la force de frappe d’un bon vieux “copier / coller” pour avoir un pipeline de CI tout beau tout propre sur notre projet. Pourquoi ne pas migrer vos stages et vos jobs dans des “catalogues de CI/CD” pour avoir l’ensemble de vos scripts à disposition sur une étagère ? Après vous avoir présenté cette approche, nous vous détaillerons la manière de la mettre rapidement en place dans GitLab. Nous verrons ses avantages et inconvénients et terminerons par parler de la solution R2DevOps qui a depuis plusieurs années créé leur solution en se basant sur cette notion de catalogue.',
+          tags: ['CICD', 'GitLab', 'R2DevOps'],
           lang: ['fr'],
         },
       ],
       cities: ['Nantes'],
       contacts: {
-        twitter: 'JPhi_Baconnais',
         mail: 'jeanphilippe.baconnais@gmail.com',
       },
     },
